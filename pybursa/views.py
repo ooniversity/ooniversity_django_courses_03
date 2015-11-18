964d4edd9e69524d6d90ca7eb1de2df3ccd650ee
Roma Makliuk <voobragariym@gmail.com>
2015-11-18 19:14:36 +0200

    add views.py to pybursa

---
+++ pybursa/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def index(request):
+    return render(request, 'index.html')
+def contact(request):
+    return render(request, 'contact.html')
+def student_list(request):
+    return render(request, 'student_list.html')
+def student_detail(request):
+    return render(request, 'student_detail.html')            
+    
