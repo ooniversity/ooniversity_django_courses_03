0860ecb4d222dc13963adb02647789d5f192c8fb
Stas mykhailov <s.m.mykhailov@gmail.com>
2015-11-18 23:28:48 +0200

    trying to link 2

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.staticfiles import static
 
 def index(request):
     return render(request, "{% url 'index' %}")
+    #return render(request, "index.html")
+    
     
 def contact(request):
     return render(request, "{% url 'contact' %}")
