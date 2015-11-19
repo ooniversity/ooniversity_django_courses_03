722dc101a890b5348ff98b58775d25a9a8c727aa
bobsley <bobsley345@mail.ru>
2015-11-19 16:11:13 +0200

    Home Task 3_2 v=1.0

--- pybursa/views.py
+++ pybursa/views.py
@@ -10,5 +10,5 @@ def student_list(request):
 	return render(request, 'student_list.html')
 
 def student_detail(request):
-	return render(request, 'student_details.html')
+	return render(request, 'student_detail.html')
 
