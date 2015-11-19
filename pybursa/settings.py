6565434c97ab406c37a65ab8e4e9f17292e5cf94
Kostyantin <k_moroz@hotmail.com>
2015-11-19 13:15:49 +0200

    add settings.py & pybursa/urls.py

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pttl7!+jimb5%$q*%vxwk_irw37qilq3ftdk5d&jm7(+34pk6('
+SECRET_KEY = '4qkky3rx)y-fbqjwfx8d2n32+l32a^+yyj^xblmux=_nqgza2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
