773387028e5d3525617d1a21a2ea51e707867802
Ekaterina Puzina <katyapuzina@gmail.com>
2015-11-19 23:04:49 +0200

    Modified files with tamplete tags

---
+++ pybursa/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from polls.models import Choice, Question
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def student_list(request):
+    return render(request, 'student_list.html')
+
+def student_detail(request):
+    return render(request, 'student_detail.html')
