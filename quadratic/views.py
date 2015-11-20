ce5c13077c73d1aaa6d7abf5a982939b4955b56e
bobsley <bobsley345@mail.ru>
2015-11-21 00:42:41 +0200

    Home Task 4 (v1.0)

--- quadratic/views.py
+++ quadratic/views.py
@@ -9,7 +9,7 @@ def quadratic_results(request):
 	#s_new = s.split('&')
 	a = request.GET.get('a')
 	b = request.GET.get('b')
-	c = request.GET.get('c')
+	c = request.GET.get('c').split('/')[0]
 	mes_a = ''
 	mes_b = ''
 	mes_c = ''
@@ -21,7 +21,7 @@ def quadratic_results(request):
 		mes_a = u'коэффициент не определён'
 		error = True
 	else:
-		if not a.isdigit():
+		if not a.isdigit() and '-' not in a:
 			mes_a = u'коэффициент не целое число'
 			error = True
 		if int(a) == 0:
@@ -31,19 +31,26 @@ def quadratic_results(request):
 	if not b:
 		mes_b = u'коэффициент не определён'
 		error = True
-	elif not b.isdigit():
+	elif not b.isdigit() and '-' not in b:
 		mes_b = u'коэффициент не целое число'
 		error = True
 	if not c:
 		mes_c = u'коэффициент не определён'
 		error = True
-	elif not c.isdigit():
+	elif not c.isdigit() and '-' not in c:
 		mes_c = u'коэффициент не целое число'
 		error = True
 	if not error:
-		a = int(a)
-		b = int(b)
-		c = int(c)
+		if '-' in a:
+			a = -int(a[1:])
+		if '-' in b:
+			a = -int(b[1:])
+		if '-' in c:
+			a = -int(c[1:])
+		else:
+			a = int(a)		
+			b = int(b)
+			c = int(c)
 		discr = get_discriminant(request,a,b,c)
 		mes_discr = u'Дискриминант: %d' % discr
 		if discr < 0:
@@ -58,5 +65,5 @@ def quadratic_results(request):
 			x2 = (-b - (b*b - 4*a*c)**(1/2.0)) / (2*a)
 			message = u'Квадратное уравнение имеет два действительных корня'
 			roots = 'x1 = %d, x2 = %d' % (x1, x2)
-	return render(request, 'templates/results.html', {'a':a, 'b':b, 'c':c, 'diskriminant':mes_discr, 'message':message, 'roots':roots, 'mes_a':mes_a, 'mes_b':mes_b, 'mes_c':mes_c})
+	return render(request, 'results.html', {'a':a, 'b':b, 'c':c, 'discriminant':mes_discr, 'message':message, 'roots':roots, 'mes_a':mes_a, 'mes_b':mes_b, 'mes_c':mes_c})
 	
