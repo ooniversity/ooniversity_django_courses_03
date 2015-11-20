f54e5f8039f059c857bf67e1c838258e6d0d646f
olegpy <oleg_kriklivets@mail.ru>
2015-11-20 17:49:56 +0200

    add template result

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def quadratic_results(request):
+    return render(request, 'quadratic/result.html')
