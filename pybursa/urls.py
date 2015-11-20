577bf677fe30151f083987b5be162623245b37b4
olegpy <oleg_kriklivets@mail.ru>
2015-11-21 00:31:14 +0200

    add function quadratic_results and return html code in template

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^contact/$', contact, name='contact'),
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
-    url(r'^quadratic/', include('quadratic.urls'), name='results'),
+    url(r'^quadratic/', include('quadratic.urls'), name='quadratic'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
 
     url(r'^admin/', include(admin.site.urls)),
--- quadratic/views.py
+++ quadratic/views.py
@@ -2,4 +2,30 @@ from django.shortcuts import render
 
 
 def quadratic_results(request):
-    return render(request, 'quadratic/result.html')
+    context = {}
+    for key in request.GET.keys():
+        try:
+            context[key] = int(request.GET[key])
+            context[key + '_type'] = 'integer'
+        except ValueError:
+            if not isinstance(request.GET[key], str):
+                context[key] = ''
+            else:
+                context[key] = str(request.GET[key])
+            context[key + '_type'] = 'string'
+
+    a = context['a']
+    b = context['b']
+    c = context['c']
+
+    if map(lambda x: isinstance(x, int), (a, b, c)) and a != 0:
+        context['square'] = b**2 - 4 * a * c
+        if context['square'] > 0:
+            context['x1'] = float((-b + context['square']**(1 / 2.0)) / (2 * a))
+            context['x2'] = float((-b - context['square']**(1 / 2.0)) / (2 * a))
+        elif context['square'] == 0:
+            context['x1'] = context['x2'] = float(-b / (2 * a))
+    else:
+        context['square'] = ''
+
+    return render(request, 'quadratic/results.html', context)
