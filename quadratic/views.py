2b1548ac564feaeaaaba3a863e4fca7eb54ac865
Orest Stetsiak <axeopa@gmail.com>
2015-11-20 22:40:26 +0200

    4.0 added some files

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,9 +1,18 @@
+from django.conf.urls import patterns, include, url
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 
 
 def quadratic_results(request):
+    print request.method
+    #print request.GET['a']
+    #print request.GET['b']
+	
+	#c = print request.GET['c']
+	
+	
+
     return render(request, 'results.html')
 
