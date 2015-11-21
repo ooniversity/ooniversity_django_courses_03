b785e2753fa968abd84d84938f94a6e85d4a35cb
d_prokopchuk <dermedont@gmail.com>
2015-11-21 21:25:42 +0200

    edit KZ_3_1

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from polls.models import Question
 from polls.models import Choice, Question
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -13,8 +12,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+   
 
 admin.site.register(Question, QuestionAdmin)
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import index
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    
+    url(r'^$', index, name='index'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
