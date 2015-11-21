f31f95c8ab67347325497fc67755963a75efbe36
Kovekser <my.ac.kovalenko@gmail.com>
2015-11-21 19:15:24 +0200

    Fixe repo with Django

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
@@ -21,5 +21,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.choice_text
