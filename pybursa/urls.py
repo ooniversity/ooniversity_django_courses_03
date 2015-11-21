8065a12e4d00575b6fd3f1c26a0f877e4e24dd5d
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-11-21 12:30:55 +0200

    Updated polls (templates, views.py)

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    #http://127.0.0.1:8000/polls/latest.html
+    #url(r'^polls/latest\.html$', 'polls.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 )
