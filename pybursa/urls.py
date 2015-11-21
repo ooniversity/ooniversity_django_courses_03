042591642447f17286bffe30f0e97e2b2dddd300
Alex Timonov <alex.timonov@gmail.com>
2015-11-21 04:42:28 +0200

    3_2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from pybursa.views import index, contact, student_list, student_detail
+
 urlpatterns = patterns('',
-                       # kz_3_1 and tutorial
                        url(r'^polls/', include('polls.urls', namespace="polls")),
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', index, name="index"),
+                       url(r'^contact/$', contact, name="contact"),
+                       url(r'^student_list/$', student_list, name="student_list"),
+                       url(r'^student_detail/$', student_detail, name="student_detail"),
 
                        )
