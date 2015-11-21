1508e89129e6c8e2645742afe74173bc07e649a7
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-11-21 16:37:51 +0200

    Added pybursa/views.py, modified pybursa/settings.py, pybursa/urls.py, templates

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+#from .views import index, student_list, student_detail, contact
+#from .views import *
+import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +13,9 @@ urlpatterns = patterns('',
     #http://127.0.0.1:8000/polls/latest.html
     #url(r'^polls/latest\.html$', 'polls.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/',  views.contact, name='contact'),
+    url(r'^student_list/', views.student_list, name='student_list'),
+    url(r'^student_detail/', views.student_detail, name='student_detail'),
 )
