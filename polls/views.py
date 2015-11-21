ce248009cb5beb843c1bf7ae4006d066d6276c10
Evgeniy Padafa <padafaevgeniy@gmail.com>
2015-11-21 21:38:24 +0200

    fix

--- polls/views.py
+++ polls/views.py
@@ -28,5 +28,18 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-	model = Question
-	template_name = 'polls/results.html'
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
