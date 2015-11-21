2a40a4a9d88bcb117f42b72565a241ddeb35a66a
Svetlana77 <asm77@ukr.net>
2015-11-22 01:39:56 +0200

    Changed files

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render
+from pybursa import views
+
+from polls.views import polls_results
+
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^student_list/$', views.student_list, name='student_list'),
+    url(r'^student_detail/$', views.student_detail, name='student_detail'),
+
+    url(r'^polls/results/$', polls_results, name='polls_results'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
