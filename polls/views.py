3f49f55b7e2fbcab1540d3311185ee7731bbdc6d
AlexI <alex.yanovskii@gmail.com>
2015-11-21 20:47:58 +0200

    +tutorials3

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from polls.models import Question
 from django.template import RequestContext, loader
+from django.http import Http404
 
 # Create your views here.
+# def index(request):
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = RequestContext(request, {
+        # 'latest_question_list': latest_question_list,
+    # })
+    # return HttpResponse(template.render(context))#    return HttpResponse("Hello, world. You're at the polls index.")
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))#    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+    # try:
+        # question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+        # raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -21,3 +38,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
