b3858bfe4a4835f2c0c7f225f5251f789d4f7bb2
d_prokopchuk <dermedont@gmail.com>
2015-11-21 22:07:33 +0200

    edit KZ_3_1

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from __future__ import unicode_literals
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
