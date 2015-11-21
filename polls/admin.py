b0f812156b7c5473fabc56b96b12cf705eee23e3
Evgeniy Padafa <padafaevgeniy@gmail.com>
2015-11-21 20:56:55 +0200

    unicode fix

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from polls.models import Choice, Question
 
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
