06890088a35f69804607a27598a56e62e0c7a587
olegpy <oleg_kriklivets@mail.ru>
2015-11-21 09:46:07 +0200

    some fix

--- quadratic/views.py
+++ quadratic/views.py
@@ -2,30 +2,30 @@ from django.shortcuts import render
 
 
 def quadratic_results(request):
-    content = {}
+    value = {}
     for key in request.GET.keys():
         try:
-            content[key] = int(request.GET[key])
-            content[key + '_type'] = 'integer'
+            value[key] = int(request.GET[key])
+            value[key + '_type'] = 'integer'
         except ValueError:
             if request.GET[key] == '':
-                content[key] = ''
+                value[key] = ''
             else:
-                content[key] = str(request.GET[key])
-            content[key + '_type'] = 'string'
+                value[key] = str(request.GET[key])
+            value[key + '_type'] = 'string'
 
-    a = content['a']
-    b = content['b']
-    c = content['c']
+    a = value['a']
+    b = value['b']
+    c = value['c']
 
     if all(map(lambda x: isinstance(x, int), (a, b, c))) and a != 0:
-        content['square'] = b**2 - 4 * a * c
-        if content['square'] > 0:
-            content['x1'] = float((-b + content['square']**(1 / 2.0)) / (2 * a))
-            content['x2'] = float((-b - content['square']**(1 / 2.0)) / (2 * a))
-        elif content['square'] == 0:
-            content['x1'] = content['x2'] = float(-b / (2 * a))
+        value['disrcim'] = b**2 - 4 * a * c
+        if value['disrcim'] > 0:
+            value['x1'] = float((-b + value['disrcim']**(1 / 2.0)) / (2 * a))
+            value['x2'] = float((-b - value['disrcim']**(1 / 2.0)) / (2 * a))
+        elif value['disrcim'] == 0:
+            value['x1'] = value['x2'] = float(-b / (2 * a))
     else:
-        content['square'] = ''
+        value['disrcim'] = ''
 
-    return render(request, 'quadratic/results.html', content)
+    return render(request, 'quadratic/results.html', value)
