0dab9d7a34c2866ba23d3bb4f92be1a1c45386d7
Alex Timonov <alex.timonov@gmail.com>
2015-11-22 18:03:27 +0200

    fixes

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from pybursa.views import index, contact, student_list, student_detail
-from quadratic.views import quadratic_results
 
 urlpatterns = patterns('',
                        url(r'^polls/', include('polls.urls', namespace="polls")),
@@ -11,6 +10,5 @@ urlpatterns = patterns('',
                        url(r'^contact/$', contact, name="contact"),
                        url(r'^student_list/$', student_list, name="student_list"),
                        url(r'^student_detail/$', student_detail, name="student_detail"),
-                       url(r'^quadratic/', quadratic_results, name='results'),
-
+                       url(r'^quadratic/', include('quadratic.urls')),
                        )
