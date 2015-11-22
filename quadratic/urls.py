a5003760093dc260550b771b03fc59d99def2b1a
Evgeniy Padafa <padafaevgeniy@gmail.com>
2015-11-22 19:08:38 +0200

    fix

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from quadratic import views
 
 urlpatterns = patterns('',
-    url(r'^results/$', views.quadratic_results, name='qer'),
+	url(r'^results/', views.quadratic_results, name='quadratic_results'),
 )
