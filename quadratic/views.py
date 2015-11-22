8ae47285164c98f98412269fd9e3a10eb5521129
Siedovolosij Serhij <sedovolosiy@gmail.com>
2015-11-22 23:33:19 +0200

    some fix

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,38 +1,66 @@
 ﻿# -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django import forms
 
-
-class EquationForm(forms.Form):
-    a = forms.IntegerField()
-    b = forms.IntegerField()
-    c = forms.IntegerField()
-
-    def clean_a(self):
-        data = self.cleaned_data['a']
-        if data == 0:
-            raise forms.ValidationError("коэффициент при первом слагаемом не может быть равным 0")
-        return data
-
-    
 def quadratic_results(request):
-    form = EquationForm(request.GET or None)
-    if form.is_valid():
-        a=form.cleaned_data["a"]
-        roots, discr = get_roots(a=form.cleaned_data["a"],
-                                 b=form.cleaned_data["b"],
-                                 c=form.cleaned_data["c"])
-        return render(request,'results.html', {'roots':roots, 'discr':discr, 'form':form, 'a':a})
-    return render(request, 'results.html', {'form': form})
-
-
-def get_discr(a,b,c):
-    return b**2 - 4*a*c
-
-
-def get_roots(a, b, c):
-    roots = []
-    discr = get_discr(a, b, c)
-    if discr >= 0 and a != 0:
-        roots = [(-b + discr**(1/2.0))/(2*a), (-b - discr**(1/2.0))/(2*a)]
-    return roots, discr
+  #s_new = s.split('&')
+  a = request.GET.get('a')
+  b = request.GET.get('b')
+  c = request.GET.get('c').split('/')[0]
+  mes_a = ''
+  mes_b = ''
+  mes_c = ''
+  mes_discr = ''
+  message = ''
+  roots = ''
+  error = False
+  if not a:
+    mes_a = u'коэффициент не определен'
+    error = True
+  else:
+    if not a.isdigit() and '-' not in a:
+      mes_a = u'коэффициент не целое число'
+      error = True
+    if int(a) == 0:
+      mes_a = u'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+      error = True
+      
+  if not b:
+    mes_b = u'коэффициент не определен'
+    error = True
+  elif not b.isdigit() and '-' not in b:
+    mes_b = u'коэффициент не целое число'
+    error = True
+  if not c:
+    mes_c = u'коэффициент не определен'
+    error = True
+  elif not c.isdigit() and '-' not in c:
+    mes_c = u'коэффициент не целое число'
+    error = True
+  if not error:
+    if '-' in a:
+      a = -int(a[1:])
+    else:
+      a = int(a)
+    if '-' in b:
+      b = -int(b[1:])
+    else:
+      b = int(b)
+    if '-' in c:
+      c = -int(c[1:])
+    else:
+      c = int(c)
+    discr = pow(b, 2.0) - 4 * a * c
+    mes_discr = u'Дискриминант: %d' % discr
+    if discr < 0:
+      message = u'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
+      roots = ''
+    elif discr == 0:
+      x1 = x2 = round(float(-b / (2.0*a)),1)
+      message = u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень:'
+      roots = 'x1 = x2 = %s' % x1
+    elif discr > 0:
+      x1 = round(float((-b + (b*b - 4*a*c)**(1/2.0)) / (2.0*a)), 1)
+      x2 = round(float((-b - (b*b - 4*a*c)**(1/2.0)) / (2.0*a)), 1)
+      message = u'Квадратное уравнение имеет два действительных корня:'
+      roots = 'x1 = %s, x2 = %s' % (x1, x2)
+  return render(request, 'results.html', {'a':a, 'b':b, 'c':c, 'discriminant':mes_discr, 'message':message, 'roots':roots, 'mes_a':mes_a, 'mes_b':mes_b, 'mes_c':mes_c})
