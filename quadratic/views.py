53e955c2c8047585c537aa2e323b6b4c3946cc9f
AlexI <alex.yanovskii@gmail.com>
2015-11-22 19:36:16 +0200

    negative values change

--- quadratic/views.py
+++ quadratic/views.py
@@ -21,10 +21,11 @@ def quadratic_results(request):
     except:
         c = None
 
-    if a == None:
+
+    if a == None or a == '':
         context['a'] = 'коэффициент не определен'
         flag_calculate_D['a'] = False
-    elif not a.isdigit():
+    elif not is_number(a):
         context['a'] = 'коэффициент не целое число'
         flag_calculate_D['a'] = False
     elif int(a) == 0:
@@ -33,24 +34,23 @@ def quadratic_results(request):
     else:
         context['a'] = int(a)
 
-    if b == None:
+    if b == None or b == '':
         context['b'] = 'коэффициент не определен'
         flag_calculate_D['b'] = False
-    elif not b.isdigit():
+    elif not is_number(b):
         context['b'] = 'коэффициент не целое число'
         flag_calculate_D['b'] = False
     else:
         context['b'] = int(b)
 
-    if c == None:
+    if c == None or c == '':
         context['c'] = 'коэффициент не определен'
         flag_calculate_D['c'] = False
-    elif not c.isdigit():
+    elif not is_number(c):
         context['c'] = 'коэффициент не целое число'
         flag_calculate_D['c'] = False
     else:
         context['c'] = int(c)
-    print flag_calculate_D
     if flag_calculate_D['a'] and flag_calculate_D['b'] and flag_calculate_D['c']:
         context['calculate_Done'] = True
         D = context['b'] ** 2 - 4 * context['a'] * context['c']
@@ -61,14 +61,21 @@ def quadratic_results(request):
             context['D'] = D
             x = -context['b'] / (2 * context['a'])
             context['solution'] =('Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: '
-                                  'x1 = x2 = %f') % x
+                                  'x1 = x2 = %.1f') % x
         else:
             context['D'] = D
             x1 = -context['b'] + D / (2 * context['a'])
             x2 = -context['b'] + D / (2 * context['a'])
-            context['solution'] =('Квадратное уравнение имеет два действительных корня: x1 = %f, x2 = %f') % (x1, x2)
+            context['solution'] =('Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f') % (x1, x2)
     return render(request, 'results.html', context)
 
 # def detail(request, question_id):
     # question = get_object_or_404(Question, pk=question_id)
     # return render(request, 'polls/detail.html', {'question': question})
+def is_number(char):
+    try:
+        int(char)
+        return_value = True
+    except:
+        return_value = False
+    return return_value
