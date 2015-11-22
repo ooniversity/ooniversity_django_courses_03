4df5f09d14e58481bdf6fd266288bb281e682f7d
Svetlana77 <asm77@ukr.net>
2015-11-22 20:27:34 +0200

    Added polls files

--- pybursa/pybursa/urls.py
+++ pybursa/pybursa/urls.py
@@ -1,21 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.http import HttpResponse
-from django.shortcuts import render
-from pybursa import views
-#from quadratic import views
-from quadratic.views import quadratic_results, quadratic_start
-
+#from .views import index, student_list, student_detail, contact
+#from .views import *
+import views
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.index, name='index'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^student_list/$', views.student_list, name='student_list'),
-    url(r'^student_detail/$', views.student_detail, name='student_detail'),
-
-    url(r'^quadratic/results/$', quadratic_results, name='quadratic_results'),
-    
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^quadratic/', include('quadratic.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^quadratic/start/$', quadratic_start, name='quadratic_start'),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/',  views.contact, name='contact'),
+    url(r'^student_list/', views.student_list, name='student_list'),
+    url(r'^student_detail/', views.student_detail, name='student_detail'),
 )
