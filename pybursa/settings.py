7b3aa15ed3f2fa0cd36102108628ab2a195e6d43
Roma Makliuk <voobragariym@gmail.com>
2015-11-25 18:40:05 +0200

    installed apps - courses, students

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -37,13 +37,15 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
-    'quadratic'
+    'quadratic',
+    'courses',
+    'students',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
