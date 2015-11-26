8964e716d34e50734caa94e23db76f10ee069c6a
Kostyantin <k_moroz@hotmail.com>
2015-11-26 23:18:43 +0200

    admin

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	filter_horizontal = ('courses',)
+	fieldsets = [
+                ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+                ('Contact info',  {'fields': ['email', 'phone', 'address', 'skype']}),
+                (None,            {'fields': ['courses']}),
+    ]
+	list_filter = ['courses']
+	search_fields = ['surname', 'email']
+	list_display = ('full_name', 'email', 'skype')
+
+
+admin.site.register(Student, StudentAdmin)
+
+
