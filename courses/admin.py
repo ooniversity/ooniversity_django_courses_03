679035b4bc2cc61bb22be0e80301276cb7449452
Roma Makliuk <voobragariym@gmail.com>
2015-11-26 13:51:23 +0200

    fix in courses/admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from courses.models import Course, Lesson
+from courses.models import Lesson, Course
 
 
-class LessonInLine(admin.TabularInline):
+class LessonInline(admin.TabularInline):
     model = Lesson
     extra = 0
 
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description')
-    inlines = [LessonInLine]
+    inlines = [LessonInline]
     search_fields = ['name']
 
 admin.site.register(Course, CourseAdmin)
