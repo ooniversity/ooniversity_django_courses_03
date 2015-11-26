0b89e2bf8dd0d9a7d83425e400ee31a4c37e9767
Denn <denn.terentyev@gmail.com>
2015-11-26 22:37:43 +0200

    KZ5 4th commit

--- students/admin.py
+++ students/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from students.models import Student
+from django.db import models
 
-# Register your models here.
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ['name_surname', 'email', 'skype']
 	search_fields = ['surname', 'email']
-	list_filter = ['courses']
-	fieldsets = [
-        ('Personal info',{'fields': ['name', 'surname', 'date_of_birth']}),
-        ('Contact info', {'fields': ['email', 'phone', 'adress', 'skype', 'course']}),
-    ]
+	list_display = ['full_name', 'email', 'skype']
+	list_filter = [ 'courses' ]
+
+	def full_name(self, objct):
+		return '{} {}'.format(objct.name, objct.surname)
+
+	fieldsets = [ ('Personal info', { 'fields' : [ 'name', 'surname', 'date_of_birth' ]} ),
+				  ('Contact info', { 'fields' : [ 'email', 'phone', 'address' , 'skype']} ),
+				  ( None, { 'fields' : [ 'courses' ]} )]
+
+	filter_horizontal = [ 'courses' ]
+
 
 admin.site.register(Student, StudentAdmin)
