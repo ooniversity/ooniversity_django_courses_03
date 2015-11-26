c39c80e505ca40fbcd5f512a70f53f81e5133e3f
Denn <denn.terentyev@gmail.com>
2015-11-26 21:50:10 +0200

    KZ5 3d commit

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from courses.models import Course, Lesson
+from django.http import HttpResponse
 
-# Create your views here.
+def detail(request, pk):
+	return render(request, 'courses/detail.html', {'course': Course.objects.get(id = pk)})
