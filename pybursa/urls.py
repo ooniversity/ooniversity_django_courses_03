5c6a2ad84ad9f8db7fe3d3a795b510a2b72ce9aa
olegpy <oleg_kriklivets@mail.ru>
2015-11-26 22:13:23 +0200

    doing one ex

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
-from .views import index, contact, student_list, student_detail
+from .views import index, contact
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^contact/$', contact, name='contact'),
-    url(r'^student_list/$', student_list, name='student_list'),
-    url(r'^student_detail/$', student_detail, name='student_detail'),
+    # url(r'^student_detail/$', student_detail, name='student_detail'),
     url(r'^quadratic/', include('quadratic.urls'), name='quadratic'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^courses/', include('courses.urls', namespace="courses")),
