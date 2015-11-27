78adbbb8b0b84c10a358691185523b086b2c7107
Stas mykhailov <s.m.mykhailov@gmail.com>
2015-11-27 04:10:03 +0200

    kz_4 11_try

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,21 @@
+
 from django.contrib import admin
 from courses.models import Course, Lesson
+from django.forms import widgets
 # -*- coding: utf-8 -*-
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    extra = 0
+    model = Lesson
+    fields = ['subject', 'description', 'order']
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name',  'short_description']
+    search_fields = ['name']
+    inlines = [LessonInline]
+
+    
+
+
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from students.models import Student
 # -*- coding: utf-8 -*-
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    def full_name(self, obstud):
+        return '%s %s' % (obstud.name, obstud.surname)
+    list_display = ['full_name',  'email', 'skype']
+    search_fields = ['surname', 'email']
+    list_filter=['courses']
+    fieldsets = (
+        ('Personal info', {
+            'fields': ('name', 'surname', 'date_of_birth')
+        }),
+        ('Contact info', {
+            
+            'fields': ('email', 'phone', 'address', 'skype')
+        }),
+        (None, {
+            
+            'fields': ('courses',)
+        }),)
+    
+admin.site.register(Student, StudentAdmin)
