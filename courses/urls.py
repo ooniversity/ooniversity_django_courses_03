e4c4a880d5ab3513f3048c230bd7f823c6f491ec
Dmitriy Korbanitskiy <korbanitskiy@gmail.com>
2015-11-27 16:52:03 +0200

    from work

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
                        url(r'^$',views.index, name='index'),
                        url(r'courses/(\d{1})/$', views.courses_info, name='courses'),
                        url(r'contact/$', views.contacts, name='contact'),
-                       url(r'student_list/$', views.student_list, name='student_list'),
-                       url(r'student_detail/$', views.student_detail, name='student_detail'),
+                       url(r'students/$', views.students, name='students'),
+
                        )
--- courses/views.py
+++ courses/views.py
@@ -12,12 +12,17 @@ def courses_info(request, num):
     lesson = models.Lesson.objects.filter(course = models.Course.objects.filter(id=num))
     return render(request, 'courses.html', {'item': item, 'lesson': lesson})
 
+
 def contacts(request):
     return render(request, 'contact.html')
 
 
-def student_list(request):
-    return render(request, 'student_list.html')
+def students(request):
+    course_id = request.GET['course_id']
+    students = models.Student.objects.filter()
+
+
+    return render(request, 'students.html', {'course_id': course_id})
 
 
 def student_detail(request):
