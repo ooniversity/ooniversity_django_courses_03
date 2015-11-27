141a9a5f3ddedd054a4b72d96aced1f953ade772
Stas mykhailov <s.m.mykhailov@gmail.com>
2015-11-28 00:43:03 +0200

    kz_6try2

--- coaches/models.py
+++ coaches/models.py
@@ -7,7 +7,7 @@ class Coach(models.Model):
     phone = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     skype = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     
     def __unicode__(self):
         return self.user.username
