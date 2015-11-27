fe1774437a915cfa99f63d5970b72e0943b59833
korbanitskiy <korbanitskiy@gmail.com>
2015-11-27 22:52:20 +0200

    before second app

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
                        url(r'^$',views.index, name='index'),
                        url(r'courses/(\d{1})/$', views.courses_info, name='courses'),
                        url(r'contact/$', views.contacts, name='contact'),
-                       url(r'students/$', views.students, name='students'),
-
+                       url(r'students/$', views.student, name='students'),
+                       url(r'students/(\d{1})$', views.student_detail, name='student_detail'),
                        )
--- courses/views.py
+++ courses/views.py
@@ -9,7 +9,7 @@ def index(request):
 
 def courses_info(request, num):
     item = models.Course.objects.get(id=num)
-    lesson = models.Lesson.objects.filter(course = models.Course.objects.filter(id=num))
+    lesson = models.Lesson.objects.filter(course=models.Course.objects.filter(id=num))
     return render(request, 'courses.html', {'item': item, 'lesson': lesson})
 
 
@@ -17,15 +17,16 @@ def contacts(request):
     return render(request, 'contact.html')
 
 
-def students(request):
-    course_id = request.GET['course_id']
-    students = models.Student.objects.filter()
+def student(request):
+    try:
+        course_id = request.GET['course_id']
+        students = models.Student.objects.filter(courses__id=course_id)
+    except:
+        students = models.Student.objects.all()
+
+    return render(request, 'students.html', {'students': students})
 
 
-    return render(request, 'students.html', {'course_id': course_id})
-
-
-def student_detail(request):
-    return render(request, 'student_detail.html')
-
-
+def student_detail(request, num):
+    student = models.Student.objects.get(id = num)
+    return render(request, 'student_detail.html', {'student': student})
