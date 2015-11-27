d6591ddfc43a7a885493469b0d89d9845c4f84a5
is <is@itsvit.org>
2015-11-27 17:27:44 +0200

    kz6 5th try

--- coaches/admin.py
+++ coaches/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from coaches.models import Coach
 
-class CoachAdmin(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'gender', 'skype', 'description')
-	search_fields = ('user__first_name', 'user__last_name', 'gender', 'skype', 'description')
-	list_filter = (
-        ('user__is_staff', admin.BooleanFieldListFilter),
-    )
 
-	def first_name(self, objct): return objct.user.first_name
-	def last_name(self, objct): return objct.user.last_name
+class CoachAdmin(admin.ModelAdmin):
+	list_display = ['first_name', 'last_name', 'gender', 'skype', 'description']
+	list_filter = (('user__is_staff', admin.BooleanFieldListFilter),)
 
 admin.site.register(Coach, CoachAdmin)
+
+
+
+
+
--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,11 @@
+# -*- coding: utf-8 -*- 
 from django.shortcuts import render
 from courses.models import Course, Lesson
-from coaches.models import Coach
+#from coaches.models import Coach
 
 def detail(request, pk):
-    return render(request, 'courses/detail.html', { 
-    	'course' : Course.objects.get(id=pk), 
-    	'lessons' : Lesson.objects.filter(course=pk),
-    	'coach' : Coach.objects.get(id=pk)}
-    	)
+    return render(request, 'courses/detail.html', {'course' : Course.objects.get(id=pk)})
+
 """
 class CourseListView(ListView):
     model = Course
