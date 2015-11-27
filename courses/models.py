f8f50660632ab2dcd10e9936f9386dd6d9e2321a
Kostyantin <k_moroz@hotmail.com>
2015-11-27 19:01:13 +0200

    new correct

--- courses/models.py
+++ courses/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from coaches.models import Coach
+
 
 class Course(models.Model):
     name = models.CharField(max_length=225)
     short_description  = models.CharField(max_length=225)
     description = models.TextField()
-    coach = models.ForeignKey(Coach, related_name='+')
-    assistant = models.ForeignKey(Coach, related_name='+')
     
     def __unicode__(self):
         return self.name
