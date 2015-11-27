79926c1e2c4fe77377d1eaeaa23fc56c873130e3
pogorelov-sergey <sergey.pogorelov.1989@mail.ru>
2015-11-27 18:42:22 +0200

    modified many files

---
+++ coaches/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from courses.models import Course
+from coaches.models import Coach
+
+
+def detail(request, pk):
+	coaches = Coach.objects.filter(id=pk)
+	return render(request, 'coaches/detail.html', {'coaches': coaches, 'courses': Course.objects.filter(coach=pk), 'assistants': Course.objects.filter(assistant=pk)})
