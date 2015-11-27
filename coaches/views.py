6fae9ff9a08f4d6a4a472d0145ac9172d1b7cb88
olegpy <oleg_kriklivets@mail.ru>
2015-11-28 01:47:54 +0200

    aaaa

--- coaches/views.py
+++ coaches/views.py
@@ -5,8 +5,4 @@ from coaches.models import Coach
 
 def detail(request, pk):
     coach = Coach.objects.get(id=pk)
-    coach.get_full_name = coach.user.get_full_name()
-    # coach.email = coach.user.email
-    # coach.courses_coach = Course.objects.filter(coach=coach)
-    # coach.courses_assistant = Course.objects.filter(assistant=coach)
     return render(request, 'coaches/detail.html', {'coach': coach})
