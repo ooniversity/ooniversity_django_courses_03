534c040a40dd0d10a0704911213eaae1adf3aef7
Andrey Rimarenko <andrey.rimarenko@gmail.com>
2015-11-27 23:36:42 +0200

    KZ 5-1 solved some problems

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from courses.models import Course, Lesson
 from students.models import Student
 
-def students(request):
+def list_view(request):
     try:
         qs= request.GET
         course = get_object_or_404(Course, pk=qs['course_id'])
@@ -17,7 +17,7 @@ def students(request):
         students = Student.objects.all()
         return render (request, 'students/list_view.html',{'course':course, 'students': students,})
 
-def students_detail(request,student_id):
+def detail(request,student_id):
     student = get_object_or_404(Student, pk=student_id)
     courses = Course.objects.filter(student__id= student_id)
     return render (request, 'students/detail.html',{'courses':courses,'student': student})
