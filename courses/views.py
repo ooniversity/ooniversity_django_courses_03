71eecd894cd748d268b781fccfca0b092ffc8614
OlenaVakulenko <send.olena@gmail.com>
2015-11-28 14:29:38 +0200

    Task#5

---
+++ courses/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from courses.models import Course
+from django.shortcuts import get_object_or_404, render
+
+
+# Create your views here.
+def main(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/main.html', {'courses': courses})
+
+def detail(request, pk):
+	course_object =  get_object_or_404(Course, pk=pk) 
+	return render(request, 'courses/detail.html', {'course_object': course_object})
