880eb83022a65fb041fbd405722803a3f46d2311
Ekaterina Puzina <katyapuzina@gmail.com>
2015-11-28 17:10:25 +0200

    Changed admin 2

--- courses/admin.py
+++ courses/admin.py
@@ -3,7 +3,6 @@ from courses.models import Course, Lesson
 
 class LessonInLine(admin.TabularInline):
     model = Lesson
-    extra = 3
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description')
