62d1c4d8b3988e8304370d3264a2d9a1e622395f
ibeltek <igorbeltek@ukr.net>
2015-11-28 15:56:09 -0500

    5.1.v2

--- students/urls.py
+++ students/urls.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 from students import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.list_view, name = "list_view"),
+    url(r'^(?P<student_id>\d+)/$', views.detail, name = "detail"),
 )
+
