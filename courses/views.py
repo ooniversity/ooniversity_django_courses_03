e68ea9cc6e349585251568b22d11ae0935735695
ibeltek <igorbeltek@ukr.net>
2015-11-28 10:40:13 -0500

    5.1.v2

---
+++ courses/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from courses.models import Course, Lesson
+
+# Create your views here.
+
+def detail (request, course_id):
+       return render (request, 'courses/detail.html')
