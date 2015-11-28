c81ad51f187c87cf758646c9627990007949a7b7
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-11-28 14:46:59 +0200

    Main page - the list of courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from courses.models import Course
+
 
 # Create your views here.
+#def index_courses(request):
+    #courses_list = Course.objects.all()
+    #context = {'courses_list': courses_list}
+    #return render(request, 'index.html', context)
+    #return render(request, 'index.html', {'courses_list': Course.objects.all()})
--- pybursa/views.py
+++ pybursa/views.py
@@ -7,11 +7,12 @@ from django.http import HttpResponse
 #from django.http import Http404
 #from django.views import generic
 #from polls.models import Choice, Question
-
+from courses.models import Course
 
 def index(request):
     #return HttpResponse("Hello, it is index.")
-    return render(request, 'index.html')
+    #return render(request, 'index.html')
+    return render(request, 'index.html', {'courses_list': Course.objects.all()})
 
 def contact(request):
     #return HttpResponse("Hello, it is contact")
