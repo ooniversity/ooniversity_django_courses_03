a9e8921fb738b993a4a9565fcb5dd5c1b70a54cc
“oleg-tim” <book1978@gmail.com>
2015-11-28 15:56:33 +0200

    21

--- polls/admin.py
+++ polls/admin.py
@@ -17,4 +17,4 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Question, QuestionAdmin)
