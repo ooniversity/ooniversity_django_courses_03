f05972ed4bb1b476be49724af3597287fe913ae5
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-11-28 15:40:51 +0200

    Course details opening from Main page (by clicking on course name)

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from django.views import generic
 from courses.models import Course
 
 
@@ -9,3 +10,7 @@ from courses.models import Course
     #context = {'courses_list': courses_list}
     #return render(request, 'index.html', context)
     #return render(request, 'index.html', {'courses_list': Course.objects.all()})
+
+class DetailView(generic.DetailView):
+    model = Course
+    template_name = 'courses/detail.html'
