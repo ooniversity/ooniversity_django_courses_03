6153d9f68fa48cea3c5af724c4cabf015f5af7c9
Andrey Rimarenko <andrey.rimarenko@gmail.com>
2015-11-28 21:36:30 +0200

    KZ 5-2

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
-
-admin.site.register(Course)
+class LessonInline (admin.StackedInline):
+    model=Lesson
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ['name','short_description']
+    search_fields = ['name']
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from students.models import Student
-
-admin.site.register(Student)
+from courses.models import Course, Lesson
+class StudentAdmin(admin.ModelAdmin):
+    list_filter = ['courses']
+    list_display = ['name', 'email', 'skype']
+    fieldsets = [('Personal info',{'fields':['name','surname','date_of_birth']}),('Contact info',{'fields':['email','phone','address','skype','courses']})]
+    search_fields = ['surname', 'email']
+admin.site.register(Student, StudentAdmin)
