ed900bc6d08af8125b0716d044a7a9e234821b92
CoreMain <d3nshapoval@gmail.com>
2015-11-28 19:45:18 +0200

    кз 5
    Я сделялЪ (вроде как)

---
+++ courses/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+from courses.models import Course, Lesson
+
+def detail(request, course_id):
+	course = Course.objects.get(id=course_id)
+	lessons = Lesson.objects.all()
+	topics = []
+	for les in lessons:
+		if les.course == course:
+			topics.append(les)
+	return render(request, 'courses/detail.html', {'course':course, 'lessons':topics})
