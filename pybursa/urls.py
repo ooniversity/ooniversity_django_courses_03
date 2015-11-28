0609e2f026ca44a5d2a849d34a00b4c149bd614d
obutenko <obutenko@mirantis.com>
2015-11-28 21:59:14 +0200

    new

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -12,6 +11,6 @@ urlpatterns = patterns('',
     url(r'^student_detail/', views.student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^courses/', include('courses.urls', namespace="courses")),
+    url(r'^courses/', include('courses.urls', namespace="courses")),    
     url(r'^students/', include('students.urls', namespace="students")),
 )
