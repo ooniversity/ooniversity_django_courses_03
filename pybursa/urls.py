41aaf928effe86866d5cfb6ade24afe448724d9c
OlenaVakulenko <send.olena@gmail.com>
2015-11-28 17:31:29 +0200

    Test fixed

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -22,7 +22,7 @@ from pybursa import views
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^1', include('courses.urls', namespace="courses")),
+    url(r'^', include('courses.urls', namespace="courses")),
     url(r'^students/', include('students.urls', namespace="students")),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^student_list/$', views.student_list, name='student_list'),
