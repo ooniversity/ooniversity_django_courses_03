b748b1f0e5476378c357101632a026d5ebda821b
AlexUA1983 <kucherenko1983@gmail.com>
2015-11-28 17:45:09 +0200

    blablabla

--- courses/admin.py
+++ courses/admin.py
@@ -4,11 +4,13 @@ from courses.models import Course, Lesson
 
 class LessonInline(admin.TabularInline):
     model = Lesson
+    extra = 0
 
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description')
     search_fields = ['name']
+    fields = ['name', 'short_description', 'description']
     inlines = [LessonInline]
 
 
