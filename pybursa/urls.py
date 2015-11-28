0f08eda31c596a099dc17bdcea0513baccb3971c
Sergey Podlipenskiy <sergey.podlipenskiy@oracle.com>
2015-11-28 19:30:32 +0000

    oooooooooooooooo

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -6,14 +6,14 @@ from pybursa import views
 urlpatterns = patterns('',
 
     url(r'^$', views.index, name='index'),
-    
+
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^contact/', views.contact, name='contact'),
-    url(r'^students/', include('students.urls', namespace = 'students')),
-    url(r'^courses/', include('courses.urls', namespace = 'courses')),
+    url(r'contact/', views.contact, name='contact'),
+    url(r'students/', include('students.urls', namespace = 'students')),
+    url(r'courses/', include('courses.urls', namespace = 'courses')),
 
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^quadratic/', include('quadratic.urls')),
+    url(r'polls/', include('polls.urls', namespace="polls")),
+    url(r'quadratic/', include('quadratic.urls')),
 
 )
