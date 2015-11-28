9d482fe705fd2269df71977843c6091a4e483774
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-11-28 17:52:23 +0200

    Course name and description displaying on details page

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 from django.views import generic
-from courses.models import Course
+from courses.models import Course, Lesson
 
 
 # Create your views here.
@@ -14,3 +14,24 @@ from courses.models import Course
 class DetailView(generic.DetailView):
     model = Course
     template_name = 'courses/detail.html'
+
+    #def get_queryset(self):
+        #return Lesson.objects.filter(subject='Lesson1')
+
+    #def detail(request):
+    #return HttpResponse("Hello, it is index.")
+    #return render(request, 'index.html')
+        #return render(request, 'courses/detail.html', {'lessons_list': Lesson.objects.all()})
+
+
+#class LessonsView(generic.ListView):
+    #template_name = 'courses/detail.html'
+    #context_object_name = 'lessons_list'
+
+    #def get_queryset(self):
+        #print Lessons.objects.filter(course_id=2)
+        #return Lessons.objects.filter(courses_id)
+
+
+#def lessons(request):
+    #return render(request, 'courses/detail.html', {'lessons_list': Lesson.objects.all()})
