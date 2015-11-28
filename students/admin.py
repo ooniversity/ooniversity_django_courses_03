01d216daa98e9ca015cc737407dc0e4efde2f725
Ostrenko <mr.ostrenko@gmail.com>
2015-11-28 23:58:22 +0200

    Project. Try 5

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_filter = ['courses']
+    search_fields = ['surname', 'email']
+    #sec = (self.name, self.surname)
+    #full_name = ' '.join(sec)
+
+admin.site.register(Student, StudentAdmin)
