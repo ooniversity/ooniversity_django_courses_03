7deaf283603ed1468c22d5196cf84fad64efa8ff
olegpy <oleg_kriklivets@mail.ru>
2015-11-28 13:51:19 +0200

    aaaaa

--- coaches/views.py
+++ coaches/views.py
@@ -6,10 +6,8 @@ from courses.models import Course
 def detail(request, pk):
     coaches = Coach.objects.get(id=pk)
     coach_courses = Course.objects.filter(coach=pk)
-    assistants = Course.objects.filter(assistant=pk)
     # assistant_courses = Course.objects.filter(assistant=pk)
     # print assistant_courses
     return render(request, 'coaches/detail.html', {
         'coaches': coaches,
-        'coach_courses': coach_courses,
-        'assistants': assistants})
+        'coach_courses': coach_courses})
