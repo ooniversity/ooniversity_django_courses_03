337cfbed466524e6165fcc89914fcb6a94364dfb
Orest Stetsiak <axeopa@gmail.com>
2015-11-28 21:10:45 +0200

    5_2 5 fix 4

--- students/admin.py
+++ students/admin.py
@@ -8,7 +8,10 @@ class StudentAdmin(admin.ModelAdmin):
     filter_horizontal = ['courses']
     search_fields = ('surname', 'email')
     list_display = ('full_name', 'email', 'skype')
-    
+    fieldsets = (('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+                 ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+                 (None, {'fields': ['courses']})
+                 )
 
 
 admin.site.register(models.Student, StudentAdmin)
