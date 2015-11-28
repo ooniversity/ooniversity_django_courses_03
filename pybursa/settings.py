0ee4f786546c7c046f08f7580d135c4c8f47cff6
Svetlana77 <asm77@ukr.net>
2015-11-28 23:11:02 +0200

    Edited files

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a=ql2+*21lld6n#hirqkyh+ae@*0y&f)*!d1*xpx-%5hyxltx2'
+SECRET_KEY = '6@9oy(_7$)dulw5%u4lkh7p5s3tu(l9p$l35*)(!i!t1ab$=8x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
