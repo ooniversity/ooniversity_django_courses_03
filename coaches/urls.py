83ec46103a040059f9d5a323e39e5081e8e5b6b6
oliayemets <oliayemets@yandex.ru>
2015-11-28 20:50:22 +0200

    kontrol6

---
+++ coaches/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^(?P<coach_id>\d+)/$', 'coaches.views.detail', name='detail'),
+    #url(r'^(?P<course_id>\d+)/$', 'courses.views.detail', name='detail'),
+    )
