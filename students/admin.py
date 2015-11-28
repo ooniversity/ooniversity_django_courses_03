420f91bb929320317d8e49b1ffa4530a309c7e3b
Orest Stetsiak <axeopa@gmail.com>
2015-11-28 21:16:12 +0200

    5_2 5 7 try

--- students/admin.py
+++ students/admin.py
@@ -4,14 +4,11 @@ import models
 
 class StudentAdmin(admin.ModelAdmin):
     list_filter = ['courses']
-    
     filter_horizontal = ['courses']
     search_fields = ('surname', 'email')
     list_display = ('full_name', 'email', 'skype')
     fieldsets = (('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
-                 ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
-                 (None, {'fields': ['courses']})
-                 )
-
+        ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+        (None, {'fields': ['courses']}))
 
 admin.site.register(models.Student, StudentAdmin)
