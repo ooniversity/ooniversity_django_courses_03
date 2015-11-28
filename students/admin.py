b24fe5ec8522e1f70692b8879d25e9476941f756
Ekaterina Puzina <katyapuzina@gmail.com>
2015-11-28 17:06:11 +0200

    Changed admin

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from students.models import Student
+from courses.models import Course, Lesson
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ('full_name', 'email', 'skype')
+    list_filter = ['courses']
+
+    fieldsets = [
+        ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+        ('Contact info',  {'fields': ['email', 'phone', 'address', 'skype']}),
+    ]
+    filter_horizontal = ('courses',)
+
+    def full_name(self, obj):
+        return obj.full_name()
+
+admin.site.register(Student, StudentAdmin)
