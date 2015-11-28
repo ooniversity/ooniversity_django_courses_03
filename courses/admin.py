0daf5f7bd55b872c3f579b625f3642122d51786a
Ostrenko <mr.ostrenko@gmail.com>
2015-11-28 23:20:59 +0200

    Project. Try 5

--- courses/admin.py
+++ courses/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from courses.models import Course, Lesson
 from students.models import Student
 
-admin.site.register(Course)
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ['name','short_description']
+    search_fields = ['name']
+
+admin.site.register(Course, CoursesAdmin)
 admin.site.register(Lesson)
