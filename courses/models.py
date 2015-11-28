34be611ee40a12b4442d5d70c8337ce90e6cb1cb
Ostrenko <mr.ostrenko@gmail.com>
2015-11-28 21:22:24 +0200

    Project. Try 4

---
+++ courses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    """Course class"""
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    description =  models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+class Lesson(models.Model):
+    """Course class"""
+    subject = models.CharField(max_length=255)
+    description =  models.TextField()
+    course = models.ForeignKey(Course)
+    order = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.subject
