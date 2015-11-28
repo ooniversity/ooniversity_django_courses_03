39ef42045291f986136667dbc0a48478a7d1166b
Orest Stetsiak <axeopa@gmail.com>
2015-11-28 21:08:57 +0200

    5_2 4 fix3

--- students/admin.py
+++ students/admin.py
@@ -4,7 +4,7 @@ import models
 
 class StudentAdmin(admin.ModelAdmin):
     list_filter = ['courses']
-    
+    filter_horizontal = ['courses']
 
 
 admin.site.register(models.Student, StudentAdmin)
