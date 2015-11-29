9cd0c01f6a19500a22f8dd12955a87a1a081f8fa
Ostrenko <mr.ostrenko@gmail.com>
2015-11-29 23:04:51 +0200

    Project. 6_1

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from coaches.models import Coach
 
-# Create your models here.
 class Course(models.Model):
     """Course class"""
     name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     description =  models.TextField()
+    coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
 
     def __unicode__(self):
         return self.name
