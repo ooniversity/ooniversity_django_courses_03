0d709352162482ee2718bad3891965f967223f94
ivnukov <dleshui@gmail.com>
2015-11-29 17:57:31 +0200

    kr6 upd

--- coaches/admin.py
+++ coaches/admin.py
@@ -5,7 +5,6 @@ from coaches.models import Coach
 class CoachAdmin(admin.ModelAdmin):
 
 	list_display = ['name', 'surname', 'gender', 'skype', 'description']
-	list_display_links = ['name', 'surname']
 	list_filter = ['user__is_staff']
 
 admin.site.register(Coach, CoachAdmin)
