6e26526211afb9300cb90517b37624dbf6bd2370
Maxim-ua <msinenkiy@gmail.com>
2015-11-29 11:33:50 +0200

    lesson 6.1 is done

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,6 @@ from courses.models import Course, Lesson
 
 
 def detail (request, course_id):
-       cd = Course.objects.get(id=course_id)
-       ll = Lesson.objects.filter(course=course_id)
-       return render (request, 'courses/detail.html',  {'course_detail': cd, 'lessons_list': ll})
+       details = Course.objects.get(id=course_id)
+       lessons = Lesson.objects.filter(course=course_id)
+       return render (request, 'courses/detail.html',  {'course_detail': details, 'lessons_list': lessons})
