1710926705800e12ac12a55d230b2b17db7cc887
bobsley <bobsley345@mail.ru>
2015-11-29 18:47:44 +0200

    H.Task (v1.2)

--- coaches/models.py
+++ coaches/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User
 
 class Coach(models.Model):
 	user = models.OneToOneField(User)
 	date_of_birth = models.DateField()
-	gender = models.CharField(max_length = 10, choices=(('M', 'Male'),('F', 'Femail')))
-	phone = models.CharField(max_length = 25, null = True, blank = True)
-	address = models.CharField(max_length = 255, null = True, blank = True)
+	gender = models.CharField(max_length = 10, choices=(('M', 'Male'),('F', 'Female')))
+	phone = models.CharField(max_length = 25)
+	address = models.CharField(max_length = 255)
 	skype = models.CharField(max_length = 255)
 	description = models.TextField()
 	
@@ -17,4 +17,4 @@ class Coach(models.Model):
 	def surname(self):
 		return self.user.last_name
 	def __unicode__(self):
-		return self.user.first_name
+		return self.user.username
