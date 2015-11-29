d02f93fea8ff989b3275dc53a011c7d65a0ad0ed
SilinMykola <silin.nickolay@gmail.com>
2015-11-29 22:08:53 +0200

    KZ 6_1 first

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from coaches.models import Coach
 
 class Course(models.Model):
     name = models.CharField(max_length = 255, help_text = u'название', verbose_name = u'Name')
     short_description = models.CharField(max_length = 255, help_text = u'краткое описание')
     description = models.TextField(help_text = u'полное описание')
+    coach = models.ForeignKey(Coach, null = True, blank = True, related_name="coach_courses")
+    assistant = models.ForeignKey(Coach, null = True, blank = True, related_name="assistant_courses")
+    
     def __unicode__(self):
         return self.name
 
@@ -13,5 +17,6 @@ class Lesson(models.Model):
     description = models.TextField()
     course = models.ForeignKey(Course)
     order = models.PositiveIntegerField()
+    
     def __unicode__(self):
         return self.subject
