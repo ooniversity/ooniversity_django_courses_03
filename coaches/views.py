3a11266e2c39d387dccedd9b33d6ba8418b6bdaf
Andrey Rimarenko <andrey.rimarenko@gmail.com>
2015-11-29 17:57:16 +0200

    KZ 6-1 assistant...

--- coaches/views.py
+++ coaches/views.py
@@ -6,5 +6,5 @@ from students.models import Student
 def detail(request, teacher_id):
     teacher = get_object_or_404(Coach, pk=teacher_id)
     course_coach = Course.objects.filter(coach_id = teacher_id)
-    course_assistent = Course.objects.filter(assistent_id = teacher_id)
-    return render (request, 'coaches/detail.html',{'teacher':teacher,'course_coach':course_coach, 'course_assistent':course_assistent, })
+    course_assistant = Course.objects.filter(assistant_id = teacher_id)
+    return render (request, 'coaches/detail.html',{'teacher':teacher,'course_coach':course_coach, 'course_assistant':course_assistant, })
--- courses/models.py
+++ courses/models.py
@@ -5,8 +5,8 @@ class Course (models.Model):
     name = models.CharField(max_length=30)
     short_description = models.CharField(max_length=600)
     description = models.TextField()
-    coach = models.ForeignKey(Coach, null = True, blank = True, related_name='coach_courses')
-    assistent = models.ForeignKey(Coach, null = True, blank = True, related_name='assistant_courses')
+    coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
     def  __unicode__(self):
 	    return self.name
 		
