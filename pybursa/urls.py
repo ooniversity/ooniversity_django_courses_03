623c89d33ce350fd460b3ca1ab1fc467dbf02638
Ostrenko <mr.ostrenko@gmail.com>
2015-11-29 21:30:00 +0200

    Project. 5_1 finished

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .views import index, contact, student_list, student_detail
+from courses.views import *
+from students.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', index, name='index'),
+    url(r'^contact/$', contact, name='contact'),
+    # url(r'^student_list/$', student_list, name='student_list'),
+    # url(r'^student_detail/$', student_detail, name='student_detail'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
+    url(r'^students/', include('students.urls', namespace='students')),
 )
--- students/views.py
+++ students/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.db import models
+import models
+from courses.models import Course
 
-# Create your views here.
+def list_view(request):
+    try:
+        course_id = request.GET['course_id']
+        students = models.Student.objects.filter(courses__id=course_id).order_by('id')
+        selection = True
+    except:
+        students = models.Student.objects.all()
+        selection = False
+
+    return render(request, 'course_students.html', {'students': students,
+        'selection': selection})
+
+
+def detail(request, student_id):
+    student = models.Student.objects.get(id = student_id)
+    return render(request, 'student_details.html', {'student': student})
