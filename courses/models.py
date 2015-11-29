d7e2737041bcf1a3f78cd017b4484cf6bae2b71c
Orest Stetsiak <axeopa@gmail.com>
2015-11-29 12:43:37 +0200

    6 corrected coaches migrated

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+#from coaches.models import Coach
+from coaches.models import Coach
 
 class Course(models.Model):
-    name = models.CharField(max_length=255)
-    short_description = models.CharField(max_length=255)
-    description = models.TextField(max_length=255)
+    name=models.CharField(max_length=255)
+    short_description=models.CharField(max_length=255)
+    description=models.TextField(max_length=255)
+    coach=models.ForeignKey(Coach, related_name='course_coach', blank=True, null=True)
+    assistant=models.ForeignKey(Coach, related_name='course_assistant', blank=True, null=True)
 
     def __unicode__(self):
         return self.name
