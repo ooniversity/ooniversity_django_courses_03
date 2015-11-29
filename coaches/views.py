27562e4436893f492917f422fb700fd18c605354
George <george@g-Aspire-7720.(none)>
2015-11-29 18:58:44 +0200

    fix4

--- coaches/views.py
+++ coaches/views.py
@@ -7,7 +7,7 @@ from students.models import Student
 
 def detail(request, c_id):
     params = {}
-    params['teacher'] = get_object_or_404(Coach, pk=c_id)    
+    params['coach'] = Coach.objects.get(id =c_id)    
     params['course_coach'] = Course.objects.filter(coach_id = c_id)
     params['course_assistant'] = Course.objects.filter(assistant_id = c_id)
     template = 'coaches/detail.html'
