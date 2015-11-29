ed4f0bdd45f22f144465d284ae5abfd762d98630
Orest Stetsiak <axeopa@gmail.com>
2015-11-29 11:57:59 +0200

    6 coaches added

---
+++ coaches/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from students.models import Student
+
+
+class Coach(models.Model):
+    user=models.OneToOneField(User)
+    date_of_birth=models.DateField()
+    gender= 'M'- 'Male', 'F' - 'Female')
+    phone=models.CharField(max_length=255)
+    address=models.CharField(max_length=255)
+    skype=models.CharField(max_length=255)
+    description=models.TextField(max_length=255)
+
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+
+    def __unicode__(self):
+        return self.user
