830ce3b3c5568168f18dbbcf018b3bf3b054ead8
AnikenRaven <d.petrushenko@hotmail.com>
2015-11-29 22:23:49 +0200

    6 fix 2

--- coaches/admin.py
+++ coaches/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
-from django.contrib.admin import BooleanFieldListFilter
 from coaches.models import Coach
 from django.contrib.auth.models import User
 
 class CoachAdmin(admin.ModelAdmin):
     list_display = ['name', 'surname', 'gender', 'email', 'skype', 'description']
-    list_filter = (('user__is_staff',BooleanFieldListFilter),)
+    list_filter = (('user__is_staff',admin.BooleanFieldListFilter),)
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'password', 'first_name', 'last_name', 'email', 'is_staff']
-    list_filter = (('is_staff',BooleanFieldListFilter),)
+    list_display = ['username', 'password', 'first_name', 'last_name', 'gender', 'email', 'skype', 'is_staff']
+    list_filter = (('is_staff', admin.BooleanFieldListFilter),)
     fields = ['username', 'password', 'first_name', 'last_name', 'email', 'is_staff']
 
+    def gender(self, obj):
+        return obj.coach.gender
+
+    def skype(self, obj):
+        return obj.coach.skype
+
+    def description(self, obj):
+        return obj.coach.description
 
 admin.site.register(Coach, CoachAdmin)
 admin.site.unregister(User)
