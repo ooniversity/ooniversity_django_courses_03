ef4218d9a692040897442c3d9b06c552e8e9d699
Orest Stetsiak <axeopa@gmail.com>
2015-11-29 12:45:01 +0200

    6 correct migration

--- courses/models.py
+++ courses/models.py
@@ -6,8 +6,8 @@ class Course(models.Model):
     name=models.CharField(max_length=255)
     short_description=models.CharField(max_length=255)
     description=models.TextField(max_length=255)
-    coach=models.ForeignKey(Coach, related_name='course_coach', blank=True, null=True)
-    assistant=models.ForeignKey(Coach, related_name='course_assistant', blank=True, null=True)
+    coach=models.ForeignKey(Coach, related_name='coach_courses', blank=True, null=True)
+    assistant=models.ForeignKey(Coach, related_name='assistant_courses', blank=True, null=True)
 
     def __unicode__(self):
         return self.name
