37edb24a55cdfbd80d04c2b878f8139041c3a397
Ostrenko <mr.ostrenko@gmail.com>
2015-11-29 23:36:49 +0200

    Project. 6_1

--- coaches/admin.py
+++ coaches/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Coach
 
-#class CoursesAdmin(admin.ModelAdmin):
-#    list_display = ['name','short_description']
-#    search_fields = ['name']
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ['user.name', 'user.last_name', 'gender', 'skype', 'description']
+    list_filter = ['user.name']
 
-admin.site.register(Coach)
+admin.site.register(Coach, CoachAdmin)
