f64cfb5c4c33cecab722a092952ec57cb662144f
OlenaVakulenko <send.olena@gmail.com>
2015-11-29 20:45:46 +0200

    Link to root corrected

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,5 @@ from courses.views import detail
 urlpatterns = patterns('',
     url(r'^courses/(?P<pk>\d+)/$', detail, name='detail'),
     url(r'^$', main, name='index'),
-    url(r'^courses/$', main, name='index'),
+    url(r'^courses/$', main, name='courses'),
 	)
