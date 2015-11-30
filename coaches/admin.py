c473a39f17bdc8a8b310023b300a2546474ade92
Ostrenko <mr.ostrenko@gmail.com>
2015-11-30 19:29:36 +0200

    First try

--- coaches/admin.py
+++ coaches/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Coach
+import models
 
 class CoachAdmin(admin.ModelAdmin):
     #list_display = ['user.name', 'user.last_name', 'gender', 'skype', 'description']
-    list_filter = ['get_name']
+    list_filter = ['user__is_staff']
+    pass
 
 admin.site.register(Coach, CoachAdmin)
