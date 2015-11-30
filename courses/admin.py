570d73a7833a01444245f722df9b8281a18e68d3
Svetlana77 <asm77@ukr.net>
2015-11-30 18:23:07 +0200

    Edited files KR6

--- courses/admin.py
+++ courses/admin.py
@@ -4,11 +4,11 @@ from courses.models import Course, Lesson
 class LessonInline(admin.TabularInline):
   model = Lesson
   fields = [ 'subject', 'description' , 'order' ]
+  extra = 0
 
 class CourseAdmin(admin.ModelAdmin):
   list_display = ['name', 'short_description']
   search_fields = ['name']
-  fields = [ 'name', 'short_description', 'description' ]
   inlines = [LessonInline]
 
 admin.site.register(Course, CourseAdmin)
