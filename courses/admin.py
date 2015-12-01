77f35d97055d8d6445028d0a0ed9dc6a563c32f0
Kirill Ushkov <kirillushkov2010@gmail.com>
2015-12-01 14:18:38 +0200

    Changed admin stylign

--- courses/admin.py
+++ courses/admin.py
@@ -4,6 +4,7 @@ from courses.models import Course,Lesson
 class LessonInline(admin.TabularInline):
 	model = Lesson
 	extra = 0
+	fields = [ 'subject', 'description' , 'order' ]
 
 class CourseAdmin(admin.ModelAdmin):
 	list_display = ['name', 'short_description']
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['full_name', 'email', 'skype']
+
+	def full_name(self, obj):
+		return obj.name + ' ' + obj.surname
+
+	full_name.short_description = 'Full name'
+
+	fieldsets = (
+			('Personal info', 
+				{'fields' : ('name', 'surname', 'date_of_birth')}
+			),
+
+			('Contact info',
+				{'fields' : ('email','phone','address','skype')}
+			),
+
+			(None,
+				{'fields' : ('courses',)}
+			),
+		)
+	filter_horizontal = ['courses']
+
+
+admin.site.register(Student, StudentAdmin)
