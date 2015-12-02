41a2e5b616a849c7c107cd795c05346623d02141
Vitalii Golovachov <golv1974@gmail.com>
2015-12-03 00:03:13 +0200

    Added files

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from courses.models import Course
 
-def index(request):
-    return render(request, 'index.html', {})
+def index (request):
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request, 'index.html', context)
 
 def contact(request):
     return render(request, 'contact.html', {})
