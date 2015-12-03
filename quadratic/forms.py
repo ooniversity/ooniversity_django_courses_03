ea5e6415c3608a9fa0f62a19d3a180de00a5c891
Ekaterina Puzina <katyapuzina@gmail.com>
2015-12-03 22:11:55 +0200

    Form quuadratic

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -2,9 +2,9 @@
 from django import forms
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField(label = "коэффициент a")
-    b = forms.IntegerField(label = "коэффициент b")
-    c = forms.IntegerField(label = "коэффициент c")
+    a = forms.IntegerField(required=True, label = "коэффициент a")
+    b = forms.IntegerField(required=True, label = "коэффициент b")
+    c = forms.IntegerField(required=True, label = "коэффициент c")
 
     """
     def clean(self):
@@ -12,17 +12,15 @@ class QuadraticForm(forms.Form):
         a = cleaned_data.get("a")
         b = cleaned_data.get("b")
         c = cleaned_data.get("c")
-
         if not a:
             self.add_error('a', "коэффициент не определен")
-
         if not b:
             self.add_error('b', "коэффициент не определен")
     """
     def clean_a(self):
         a = self.cleaned_data['a']
 
-        if a == 0.0:
+        if a == 0:
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
 
         return a
--- quadratic/views.py
+++ quadratic/views.py
@@ -12,50 +12,48 @@ def get_discr (a, b, c):
 def quadratic_results(request):
 
     contex = {}
-    #form = QuadraticForm()
-    #if request.method == "GET":
-    form = QuadraticForm(request.GET)
-    if form.is_valid():
-        #a = int(request.GET['a'])
-        #b = int(request.GET['b'])
-        #c = int(request.GET['c'])
-        a = form.cleaned_data['a']
-        b = form.cleaned_data['b']
-        c = form.cleaned_data['c']
+    if request.method == "GET":
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+            #a = int(request.GET['a'])
+            #b = int(request.GET['b'])
+            #c = int(request.GET['c'])
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            c = form.cleaned_data['c']
 
-        contex['a'] = a
-        contex['b'] = b
-        contex['c'] = c
+            contex['a'] = a
+            contex['b'] = b
+            contex['c'] = c
 
-        par_d = ""
-        solution = ""
+            par_d = ""
+            solution = ""
 
                 #if not a_error and not b_error and not c_error:
-        d = get_discr(a,b,c)
-        par_d = "Дискриминант: %d" %d
+            d = get_discr(a,b,c)
+            par_d = "Дискриминант: %d" %d
 
-        if d<0:
-            solution = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-        elif d==0:
-            x1= round(-b/2.0*a, 1)
-            solution = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f" % x1
-        else:
-            x1=(-b+d**(1/2.0))/(2*a)
-            x2=(-b-d**(1/2.0))/(2*a)
-            solution = "Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f" % (x1, x2)
-        contex['d'] = par_d
-        contex['solution'] = solution
+            if d<0:
+                solution = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+            elif d==0:
+                x1= round(-b/2.0*a, 1)
+                solution = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f" % x1
+            else:
+                x1=(-b+d**(1/2.0))/(2*a)
+                x2=(-b-d**(1/2.0))/(2*a)
+                solution = "Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f" % (x1, x2)
+            contex['d'] = par_d
+            contex['solution'] = solution
     else:
         form = QuadraticForm()
 
     contex['form'] = form
+    print contex
 
     """
         errors = False
-
         a = request.GET['a']
         a_error = ""
-
         if a:
             p = a.replace("-", "")
             if p.isdigit():
@@ -67,10 +65,8 @@ def quadratic_results(request):
                 a_error = "коэффициент не целое число"
         else:
             a_error="коэффициент не определен"
-
         b = request.GET['b']
         b_error = ""
-
         if b:
             p = b.replace("-", "")
             if p.isdigit():
@@ -79,10 +75,8 @@ def quadratic_results(request):
                 b_error = "коэффициент не целое число"
         else:
             b_error="коэффициент не определен"
-
         c = request.GET['c']
         c_error = ""
-
         if c:
             p = c.replace("-", "")
             if p.isdigit():
@@ -91,14 +85,11 @@ def quadratic_results(request):
                 c_error = "коэффициент не целое число"
         else:
             c_error="коэффициент не определен"
-
         par_d = ""
         solution = ""
-
         if not a_error and not b_error and not c_error:
             d = get_discr(a,b,c)
             par_d = "Дискриминант: %d" %d
-
             if d<0:
                 solution = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
             elif d==0:
@@ -109,7 +100,6 @@ def quadratic_results(request):
                 x1=(-b+d**(1/2.0))/(2*a)
                 x2=(-b-d**(1/2.0))/(2*a)
                 solution = "Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f" % (x1, x2)
-
         return render(request, 'quadratic/results.html', {'a': a, 'a_error': a_error, 'b': b, 'b_error': b_error, 'c': c,
             'c_error': c_error,'d': par_d, 'solution': solution, 'form': form})
     """
