b69a1b6d273c6b4e5688460429c6e0fba8aa55c0
is <is@itsvit.org>
2015-12-04 00:52:57 +0200

    kz7_1 list

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.list_view, name='list_view'),
+    url(r'^$', views.list_view, name='list'),
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
     url(r'^add/$', views.create, name='add'),
     url(r'^edit/$', views.edit, name='edit'),
--- students/views.py
+++ students/views.py
@@ -9,7 +9,7 @@ def list_view(request):
         students=Student.objects.filter(courses=r['course_id'])
     else:
         students=Student.objects.all()
-    return render(request, 'students/list_view.html', {'students': students})
+    return render(request, 'students/list.html', {'students': students})
 
 
 def create(request):
