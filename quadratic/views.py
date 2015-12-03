0f1012d5f5bb89b09a95fda30fd105b332ea254b
olegpy <oleg_kriklivets@mail.ru>
2015-12-03 16:24:06 +0200

    change example 1

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,39 +1,48 @@
 from django.shortcuts import render
 
-from forms import QuadraticForm
+from quadratic.forms import QuadraticForm
 
 
 def quadratic_results(request):
-    form = QuadraticForm()
+    # print 'a'
+    # form = QuadraticForm()
     context = {}
-    for key in request.GET.keys():
-        if request.GET[key] == '':
-            context[key] = ''
-            context[key + '_type'] = 'string'
-        else:
-            try:
-                context[key] = int(request.GET[key])
-                context[key + '_type'] = 'integer'
-            except Exception:
-                context[key] = str(request.GET[key])
-                context[key + '_type'] = 'string'
+    # a, b, c = '', '', ''
+    # for key in request.GET.keys():
+    #     if request.GET[key] == '':
+    #         context[key] = ''
+    #         context[key + '_type'] = 'string'
+    #     else:
+    #         try:
+    #             context[key] = int(request.GET[key])
+    #             context[key + '_type'] = 'integer'
+    #         except Exception:
+    #             context[key] = str(request.GET[key])
+    #             context[key + '_type'] = 'string'
 
-    a = context['a']
-    b = context['b']
-    c = context['c']
+    if request.GET.get('a') != None:
+        # print 'aa'
+        form = QuadraticForm(request.GET)
+        context['form'] = QuadraticForm(request.GET)
+        if form.is_valid():
 
-    if all(map(lambda x: isinstance(x, int), (a, b, c))) and a != 0:
-        context['disrcim'] = b**2 - 4 * a * c
-        if context['disrcim'] > 0:
-            context['x1'] = float(
-                (-b + context['disrcim']**(1 / 2.0)) / (2 * a))
-            context['x2'] = float(
-                (-b - context['disrcim']**(1 / 2.0)) / (2 * a))
-        elif context['disrcim'] == 0:
-            context['x1'] = context['x2'] = float(-b / (2 * a))
+            if form.clean_a():
+                data = form.cleaned_data
+                # print data['a']
+
+                context['disrcim'] = data['b']**2 - 4 * data['a'] * data['c']
+                # print context['disrcim']
+                if context['disrcim'] > 0:
+                    context['x1'] = float(
+                        (-data['b'] + context['disrcim']**(1 / 2.0)) / (2 * data['a']))
+                    context['x2'] = float(
+                        (-data['b'] - context['disrcim']**(1 / 2.0)) / (2 * data['a']))
+                elif int(context['disrcim']) == 0:
+                    context['x1'] = context[
+                        'x2'] = float(-data['b'] / (2 * data['a']))
+
+        return render(request, 'quadratic/results.html', context)
     else:
-        context['disrcim'] = ''
+        context['form'] = QuadraticForm()
 
-    context['form'] = form
-
-    return render(request, 'quadratic/results.html', context)
+        return render(request, 'quadratic/results.html', context)
