483f84f2d6c28f1b0528fd3a2b01ff7df127feb5
bobsley <bobsley345@mail.ru>
2015-12-03 20:10:19 +0200

    H.Task 7_2 (v1.0)

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
 from django import forms
 
 class QuadraticForm(forms.Form):
-	a = forms.FloatField()
-	b = forms.FloatField()
-	c = forms.FloatField()
+	a = forms.FloatField(label="коэффициент a:")
+	b = forms.FloatField(label="коэффициент b:")
+	c = forms.FloatField(label="коэффициент c:")
+	
+	def clean_a(self):
+		a = self.cleaned_data['a']
+		if a == 0:
+			raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+		return a
