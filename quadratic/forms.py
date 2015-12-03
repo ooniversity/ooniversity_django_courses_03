8bc7ea4076f56c9ec729499a95bdbff2e5d33967
Orest Stetsiak <axeopa@gmail.com>
2015-12-04 00:33:49 +0200

    5_2 now work

---
+++ quadratic/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class QuadraticForm(forms.Form):
+    a = forms.FloatField()
+    b = forms.FloatField()
+    c = forms.FloatField()
+
+    def clean_a(self):
+        a = self.cleaned_data['a']
+        if a == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return self.cleaned_data['a']
