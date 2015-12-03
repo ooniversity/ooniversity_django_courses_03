58b14d20fd40edeba49baa22ec12369a3375374c
oliayemets <oliayemets@yandex.ru>
2015-12-03 11:00:08 +0200

    kontrol 7_2 change3

--- coaches/views.py
+++ coaches/views.py
@@ -4,7 +4,8 @@ from courses.models import Course
 
 # Create your views here.
 def detail(request, coach_id):
+	coaches = Coach.objects.all()
 	coach = Coach.objects.get(id = coach_id)
 	course_coach = Course.objects.filter(coach_id = coach_id)
 	course_assistant = Course.objects.filter(assistant_id = coach_id)
-	return render(request, 'coaches/detail.html', {'coach':coach, 'course_coach':course_coach, 'course_assistant':course_assistant})
+	return render(request, 'coaches/detail.html', {'coaches':coaches, 'coach':coach, 'course_coach':course_coach, 'course_assistant':course_assistant})
