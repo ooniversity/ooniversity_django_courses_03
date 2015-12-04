9cdb292d0d651cdc46f9b363cf87c3b2f81f84fe
pogorelov-sergey <sergey.pogorelov.1989@mail.ru>
2015-12-04 16:12:50 +0200

    modified form for students

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from students.models import Student
 from courses.models import Course
-from students.forms import StudentForm
+from students.forms import StudentModelForm
 from django.contrib import messages
 
 	
@@ -22,7 +22,7 @@ def detail(request, pk):
 	
 def create(request):
 	if request.method == 'POST':	
-		model_form = StudentForm(request.POST)
+		model_form = StudentModelForm(request.POST)
 		if model_form.is_valid():
 			application = model_form.save()
 			name = model_form.cleaned_data['name']
@@ -30,20 +30,20 @@ def create(request):
 			messages.success(request, 'Student %s %s has been successfully added.' % (name, surname))
 			return redirect('students:list_view')
 	else:
-		model_form = StudentForm()
+		model_form = StudentModelForm()
 	return render(request, 'students/add.html', {'model_form': model_form})
 
 
 def edit(request, pk):
 	application = Student.objects.get(id=pk)
 	if request.method == 'POST':	
-		model_form = StudentForm(request.POST, instance=application)
+		model_form = StudentModelForm(request.POST, instance=application)
 		if model_form.is_valid():
 			application = model_form.save()
 			messages.success(request, 'Info on the student has been sucessfully changed.')
 			return redirect('students:edit', application.id)
 	else:
-		model_form = StudentForm(instance=application)
+		model_form = StudentModelForm(instance=application)
 	return render(request, 'students/edit.html', {'model_form': model_form})
 	
 
