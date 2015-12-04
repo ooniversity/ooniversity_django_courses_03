418b21bb427bb63288cec1aa7f46e57cb013b24d
oliayemets <oliayemets@yandex.ru>
2015-12-04 15:50:39 +0200

    kontrol 7_3 added1

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
 	url(r'^(?P<student_id>\d+)/$', 'students.views.detail', name='detail'),
     url(r'^$', 'students.views.list_view', name='list_view'),
-    url(r'^add/$', 'students.views.create', name='create'),
+    url(r'^add/$', 'students.views.create', name='add'),
     url(r'^edit/(?P<student_id>\d+)/$', 'students.views.edit', name='edit'),
     url(r'^remove/(?P<student_id>\d+)/$', 'students.views.remove', name='remove'),
     )
