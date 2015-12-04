f709aa2c79046f185661345c3ae7f85d59dea575
Kirill Ushkov <kirillushkov2010@gmail.com>
2015-12-04 20:23:14 +0200

    added model form add/remove/upd

---
+++ students/form.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from students.models import Student
+
+class StudentModelForm(ModelForm):
+	class Meta:
+		model = Student
+
--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from students.models import Student
+from students.form import StudentModelForm
+
 
 def list_view(request):
 	course_id = request.GET.get('course_id', None)
@@ -18,3 +20,37 @@ def detail(request, student_id):
 	student = get_object_or_404(Student, pk=student_id)
 	context = {'student' : student}
 	return render(request, './students/detail.html', context)
+
+def create(request):
+	context = {}
+	if request.POST:
+	    form = StudentModelForm(request.POST)
+	    if form.is_valid():
+			form.save()
+			data = form.cleaned_data
+			msg = 'Student %s %s has been successfully added.' % (data['name'], data['surname'])
+			return redirect('students:list_view', {'message': msg})
+	else:
+	    form = StudentModelForm()
+	return render(request, './students/add.html', { 'form' : form })
+
+def edit(request, student_id):
+	student = Student.objects.get(id = student_id)
+	if request.POST:
+	    form = StudentModelForm(request.POST, instance = student)
+	    if form.is_valid():
+	    	data = form.cleaned_data
+	    	msg = 'Student %s %s has been successfully edited.' % (data['name'], data['surname'])
+	    	form = Student.objects.get(id = student_id)
+	    	return render(request, './students/edit.html', {'form' : form, 'message' : msg})
+	    else:
+			form = StudentModelForm(instance = student)
+	return render(request, './students/edit.html', { 'form' : form } )
+
+def remove(request, student_id):
+	student = Student.objects.get(id = student_id)
+	if request.method == "POST":
+	    student.delete()
+	    return redirect('students:list_view')
+	return render(request, './students/remove.html', { 'student' : student })
+
