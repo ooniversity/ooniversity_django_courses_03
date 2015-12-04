b94702335e5c89dbca2c4321630785da4cdfd4c6
Alexander Babichuk <Oleksandr.Babichuk@gmail.com>
2015-12-04 11:58:52 +0200

    Mockup-37

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from __future__ import unicode_literals
 
-
-class MyntegerField(forms.IntegerField):
-    default_error_messages = {
-        'invalid': 'Enter a whole number.',
-    }
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control input-sm', 'placeholder': 'Enter a:'}))
-    b = forms.IntegerField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control input-sm', 'placeholder': 'Enter b:'}))
-    c = forms.IntegerField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control input-sm', 'placeholder': 'Enter c:'}))
+    a = forms.IntegerField(label='коэффициент a')
+    b = forms.IntegerField(label='коэффициент b')
+    c = forms.IntegerField(label='коэффициент c')
 
     def clean_a(self):
         data = self.cleaned_data['a']
