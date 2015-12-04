8c132ea5bfcb34bec5279d8fab9e5628a1d12710
ShchutskayaElena <ENLevchenko@gmail.com>
2015-12-04 23:45:04 +0200

    7.2.4

--- quadratic/views.py
+++ quadratic/views.py
@@ -11,26 +11,27 @@ def quadratic_results(request):
 	if request.method == "GET":
 		form = QuadraticForm(request.GET)	
 		if form.is_valid():
+			print 'valid'
 			a = form.cleaned_data['a']
 			b = form.cleaned_data['b']
 			c = form.cleaned_data['c']
-			x1 = 0
-			x2 = 0
 			d = get_discr(a,b,c)
 			if d==0:
+				print 'd==0'
 				x1 = x2 = (-1)*b/2*a
 				dic['x1'] = round(x1, 1)
 				dic['x2'] = round(x1, 1)
 				dic['d'] = d
-			elif d>0: 
+			elif d<0 and a!=0: 
+				print a
+				dic['d'] = d
+			else:
 				x1=(-b + d**(1/2.0))/(2.0*a)
 				x2=(-b - d**(1/2.0))/(2.0*a)
 				dic['x1'] = x1
 				dic['x2'] = x2
 				dic['d'] = d
-			else:
-				dic['d'] = d
 	else:
 		form = QuadraticForm()
-	
+	print dic	
 	return render(request,'quadratic/results.html', {'dic':dic, 'form':form})
