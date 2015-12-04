0e10b5cd9701a9cf5991c305d8c40d8035fe3378
olegpy <oleg_kriklivets@mail.ru>
2015-12-05 00:40:13 +0200

    8 ex

--- courses/views.py
+++ courses/views.py
@@ -43,7 +43,7 @@ def edit(request, pk):
         if form.is_valid():
             course = form.save()
             messages.success(
-                request, 'The changes has been saved.')
+                request, 'The changes have been saved.')
             return redirect('courses:edit', pk)
     else:
         form = CourseModelForm(instance=course)
