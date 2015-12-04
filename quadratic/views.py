77599b536a047e76f66fbaec51cef9dc898ed506
oliayemets <oliayemets@yandex.ru>
2015-12-04 09:28:23 +0200

    kontrol 7_3

--- quadratic/views.py
+++ quadratic/views.py
@@ -19,10 +19,10 @@ def quadratic_results(request):
 	#form = QuadraticForm()
 	if request.method == "GET":
 		form = QuadraticForm(request.GET or None)
-		if (a.lstrip('-').isdigit() and b.replace('-', '').isdigit() and c.replace('-', '').isdigit() and (a != '0')) == True:
-			a = int(a)
- 			b = int(b)
- 			c = int(c)
+		if form.is_valid():
+			a = form.cleaned_data['a']
+			b = form.cleaned_data['b']
+			c = form.cleaned_data['c']
 			d = b**2 - 4*a*c
 			#d=round(d, 1)
 			result_d = 'Дискриминант: %s' %(d)
@@ -35,6 +35,11 @@ def quadratic_results(request):
 				result = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s' %(x)
 			elif d < 0:
 				result = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
+		elif a or b or c == str:
+			form['a'] = a
+			form['b'] = b
+			form['c'] = c
+			result = 'коэффициент не целое число'
 	else:
 		form = QuadraticForm()
 	return render(request, 'quadratic/results.html', {'form':form, 'result_d':result_d, 'result':result, 'error_o':error_o, 'error_a':error_a, 'error_b':error_b, 'error_c':error_c})
