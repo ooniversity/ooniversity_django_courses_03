67621725f83f6665265b447bffd59518a04fda27
Svetlana77 <asm77@ukr.net>
2015-12-04 21:40:40 +0200

    Edited files KR7

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,12 +1,13 @@
+# -*- coding:UTF-8 -*-
 from django import forms
 
 class QuadraticForm(forms.Form):
-  a = forms.IntegerField(label="coefficient a")
-  b = forms.IntegerField(label="coefficient b")
-  c = forms.IntegerField(label="coefficient c")
+  a = forms.IntegerField(label="коэффициент a")
+  b = forms.IntegerField(label="коэффициент b")
+  c = forms.IntegerField(label="коэффициент c")
   
   def clean_a(self):
     a = self.cleaned_data['a']
     if a == 0:
-      raise forms.ValidationError("Coefficient of first term of equation can not be zero")
+      raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
     return self.cleaned_data['a']
