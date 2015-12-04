22d808b4560d32ac51c3725b3f113224fe84c0b1
SilinMykola <silin.nickolay@gmail.com>
2015-12-04 11:32:06 +0200

    KZ 7_2 first try

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
-from forms import QuadraticForm
+from quadratic.forms import QuadraticForm
 
 def discr(request,a,b,c):
   a=float(a)
@@ -10,50 +9,29 @@ def discr(request,a,b,c):
   d=(b*b)-(4*a*c)
   return d
 
-def tworoots(request, a, b, c):
-  a = float(a)
-  b = float(b)
-  c = float(c)
-  x1=(-b+((b*b-4*a*c)**(1/2.0)))/2*a
-  x2=(-b-((b*b-4*a*c)**(1/2.0)))/2*a
-  x1=round(x1,2)
-  x2=round(x2,2)
-  t=u'Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s ' % (x1,x2)
-  return t
-
-def oneroot(request, a, b, c):
-  x=-float(b)/(2*float(a))
-  t=u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s' % x
-  return t
-
 def quadratic_results(request):
-  form = QuadraticForm()
-  a = request.GET['a']
-  b = request.GET['b']
-  c = request.GET['c']
-  koef = {'a': a, 'b': b, 'c': c}
-  answer = {}
-  for i in koef.keys():
-    if not koef[i]:
-      k = 'answer_' + i
-      answer[k] = u'коэффициент не определен'
+    answer = {}
+    massage = None
+    if request.GET:
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+          a = form.cleaned_data['a']
+          b = form.cleaned_data['b']
+          c = form.cleaned_data['c']
+          d= discr(request, a, b, c)
+          if d < 0:
+            message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+          elif d == 0:
+            x = round((-b + d**(1/2.0)) / 2*a, 1)
+            message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x
+          else:
+            x1 = round((-b + d**(1/2.0)) / 2*a, 1)
+            x2 = round((-b - d**(1/2.0)) / 2*a, 1)
+            message = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1,x2)
+          
+          answer['d'] = 'Дискриминант: %d'  %d
+          answer['message'] = message
     else:
-      try:
-        koef[i] = int(koef[i])
-      except Exception:
-        k = 'answer_' + i
-        answer[k] = u'коэффициент не целое число'
-  if koef['a'] == 0:
-    answer['answer_a'] = u'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-  if len(answer) == 0:
-    d = answer['answer_d']=discr(request,a,b,c)
-    if d == 0:
-      answer['xx'] = oneroot(request, a, b, c)
-    elif d < 0:
-      answer['error_d'] = u'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
-    else:
-      answer['xx'] = tworoots(request, a, b, c)
-  answer.update(koef)
-  answer['form'] = form
-  return render(request,'results.html', answer)
-
+        form = QuadraticForm()
+    answer['form'] = form
+    return render(request,'quadratic/results.html', answer)
