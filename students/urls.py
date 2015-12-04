9a9940d34a8f5fffcef9bcf0d5bf72efad9db451
Ekaterina Puzina <katyapuzina@gmail.com>
2015-12-05 01:54:09 +0200

    Added students forms 1

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,9 @@ from students import views
 
 urlpatterns = patterns('',
     url(r'^$', views.list_view, name="list_view"),
-    url(r'^(?P<student_id>[1-9]+)/', views.detail, name="detail"),
+    url(r'^(?P<student_id>[0-9]+)/', views.detail, name="detail"),
+    url(r'^edit/(?P<student_id>\d+)/', views.edit, name="edit"),
+    url(r'^add/', views.create, name="add"),
+    url(r'^remove/(?P<student_id>\d+)/', views.remove, name="remove"),
     url(r'^admin/', include(admin.site.urls)),
 )
