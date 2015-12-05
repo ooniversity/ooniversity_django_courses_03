bf03b1d30776061989eaba8b4941904605555e59
Ostrenko <mr.ostrenko@gmail.com>
2015-12-05 22:33:14 +0200

    7_2 Third try

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
+    url(r'^add/', create, name="add"),
+    url(r'^edit/(?P<student_id>\d+)/$', edit, name="edit"),
+    url(r'^remove/(?P<student_id>\d+)/$', remove, name="remove"),
     url(r'^(?P<student_id>\d+)/$', detail, name='detail'),
     url(r'^', list_view, name='list_view'),
 )
