64caf75b65df36b2d6e43b22265d534721edbff1
OlenaVakulenko <send.olena@gmail.com>
2015-12-05 17:55:05 +0200

    Template path corrected

--- quadratic/views.py
+++ quadratic/views.py
@@ -9,12 +9,9 @@ def quadratic_results(request):
     x1 = x2 = descr = 0
     if not request.GET:
         form = QuadraticForm()
-        return render(request, 'results.html', {'form': form})
+        return render(request, 'quadratic/results.html', {'form': form})
     else:
         form = QuadraticForm(request.GET)
-        a = request.GET.get('a')
-        #if a==u'0':
-            #errors['a'] = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
         if not form.errors:
             a = int(request.GET.get('a'))
             b = int(request.GET.get('b'))
@@ -30,11 +27,13 @@ def quadratic_results(request):
             else:
                 message = 'Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f' % (x1, x2)
             descr = "Дискриминант: " + str(descr)
-            return render(request, 'results.html', {'message': message,'errors': errors, 
-        'x1': x1, 'x2': x2, 'a': a, 'b': b, 'c': c, 'descr': descr, 'form': form})
-        
-
+            return render(request, 'quadratic/results.html', {
+                'message': message, 
+                'x1': x1, 
+                'x2': x2, 
+                'descr': descr, 
+                'form': form})
         else:
-            return render(request, 'results.html', {'form': form})
+            return render(request, 'quadratic/results.html', {'form': form})
     
   
