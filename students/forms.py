d9c99b219419a0e057febd3737081aa63d452236
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-05 23:46:14 +0200

    Changed Form on ModelForm

--- students/forms.py
+++ students/forms.py
@@ -6,14 +6,16 @@ from django.contrib import messages
 # Create your views here.
 
 
-class StudentModelForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    surname = forms.CharField(max_length=50)
-    date_of_birth = forms.DateField()
-    email = forms.EmailField()
-    phone = forms.CharField(max_length=20)
-    address = forms.CharField(max_length=100)
-    skype = forms.CharField(max_length=50)
+class StudentModelForm(forms.ModelForm):
+    class Meta:
+        model = Student
+    #name = forms.CharField(max_length=50)
+    #surname = forms.CharField(max_length=50)
+    #date_of_birth = forms.DateField()
+    #email = forms.EmailField()
+    #phone = forms.CharField(max_length=20)
+    #address = forms.CharField(max_length=100)
+    #skype = forms.CharField(max_length=50)
     #courses = forms.ManyToManyField(Course)
 
 
@@ -43,6 +45,7 @@ def create(request):
             student.phone = data['phone']
             student.address = data['address']
             student.skype = data['skype']
+            #student.courses = data['courses']
             student.save()
             messages.success(request, '%s %s was successfully added' % (student.name, student.surname))
             return redirect('students:list')
