46ddfdfec6cf93d7f3807ecc64fe9cd73b904097
san <san@local>
2015-12-05 23:02:17 +0200

    (7.3) - (---) - Added edit and remove functionality

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
                        url(r'^$', views.list_view, name="list_view"),
                        url(r'^(?P<student_id>\d+)/$', views.detail, name="detail"),
                        url(r'^add/$', views.create, name="add"),
-                       url(r'^edit/(?P<pk>\d+)/$', views.edit, name="edit"),
-                       url(r'^remove/(?P<pk>\d+)/$', views.remove, name="remove"),
+                       url(r'^edit/(?P<student_id>\d+)/$', views.edit, name="edit"),
+                       url(r'^remove/(?P<student_id>\d+)/$', views.remove, name="remove"),
                        )
