775c67c5e7550db3ec620807c7d00917f46f4703
CoreMain <d3nshapoval@gmail.com>
2015-12-05 23:53:44 +0200

    кз 7_3
    111

--- students/urls.py
+++ students/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from students.views import list_view, detail
+from students.views import list_view, detail, create, edit, remove
 
 urlpatterns = patterns('',
 	
 	url(r'^(?P<student_id>\d)/$', detail, name="detail"),
 	url(r'^', list_view, name="list_view"),
-	
+	url(r'^add/$', create, name="add"),	
+	url(r'^edit/(?P<student_id>\d+)/$', edit, name="edit"),
+	url(r'remove/(?P<student_id>\d+)/$', remove, name="remove"),
 	
 )
