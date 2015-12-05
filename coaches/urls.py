bf5b612c302152061dfb0d1b23adff7d4e325ca0
Vadim Zinchenko <mr.zinchenkov@gmail.com>
2015-12-05 23:24:57 +0200

    7.1!

--- coaches/urls.py
+++ coaches/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from coaches import views
 
-urlpatterns = ('',
+urlpatterns = patterns ('',
     url(r'^(?P<pk>\d+)/$', views.detail, name = 'detail'),
 )
