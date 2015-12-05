a440f981cda9ed2721694898ae8e964e21088a76
san <san@local>
2015-12-05 21:32:07 +0200

    (7.3) - (---) - Added add functionality (not tested)

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.list_view, name = "list_view"),
-    url(r'^(?P<student_id>[1-5]{1})/$', views.detail, name = "detail"),
-)
+                       url(r'^$', views.list_view, name="list_view"),
+                       url(r'^(?P<student_id>[1-5]{1})/$', views.detail, name="detail"),
+                       url(r'^add/$', views.create, name="add"),
+                       )
--- students/views.py
+++ students/views.py
@@ -1,12 +1,13 @@
 # -*- coding:UTF-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from students.models import Student
 from courses.models import Course
+from students.forms import StudentModelForm
+from django.contrib import messages
 
 
 def list_view(request):
-
     get_course_id = request.GET.get('course_id', None)
 
     courses_list = Course.objects.all()
@@ -19,11 +20,24 @@ def list_view(request):
 
         students_list = Student.objects.all()
 
-    return render(request,'students/list.html', {'students_list': students_list, 'courses_list': courses_list})
+    return render(request, 'students/list.html', {'students_list': students_list, 'courses_list': courses_list})
 
 
 def detail(request, student_id):
-
     student_details = Student.objects.get(id=student_id)
 
-    return render(request,'students/detail.html', {'student_details': student_details})
+    return render(request, 'students/detail.html', {'student_details': student_details})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            form_content = form.cleaned_data
+            form.save()
+            notification = "Студент %s %s успешно добавлен" % (form_content['name'], form_content['surname'])
+            messages.success(request, notification)
+        return redirect('students:list_view')
+    else:
+        form = StudentModelForm()
+    return render(request, 'students/add.html', {'form': form})
