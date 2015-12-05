1165b5727d539f266e054b207617d92787269e1b
George <george@g-Aspire-7720.(none)>
2015-12-05 22:32:18 +0200

    fix4

--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from courses.models import Course
 from students.models import Student
@@ -17,3 +17,37 @@ def list_view(request):
     else:
         params['students'] = Student.objects.all()
     return render(request, 'students/list.html', params)
+
+def create(request):
+    params = {}
+    if request.POST:
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            cleaned = form.cleaned_data
+            messages.success(request, 'Student {0} {1} has been successfully added.'.format(cleaned['name'], cleaned['surname']))
+        return redirect('students:list_view')
+    else:
+        params['form'] = StudentModelForm()
+    return render(request, 'students/add.html', params)
+
+def edit(request, s_id):
+    params = {}
+    student = Student.objects.get(id = student_id)
+    if request.POST:
+        form = StudentModelForm(request.POST, instance = student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Info on the student has been sucessfully changed.')
+    else:
+        params['form'] = StudentModelForm(instance = student)
+    return render(request, 'students/edit.html', params )
+
+def remove(request, s_id):
+    params = {}
+    params['student'] = Student.objects.get(id = student_id)
+    if request.method == "POST":
+        student.delete()
+        messages.success(request, "Info on %s %s has been sucessfully deleted." % (student.name, student.surname))
+        return redirect('students:list_view')
+    return render(request, 'students/remove.html', params)
