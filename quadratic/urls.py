4e149af1148158a8555f6fc642db548477010b85
cadet <nazarenko.volodymyr@gmail.com>
2015-12-05 12:41:42 +0200

    kz 7.1 modified hard 1.6

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from quadratic import views
 
 urlpatterns = patterns('',
-    url(r'^results/$', views.quadratic_results, name='quadratic_results'),
+    url(r'results/$', views.quadratic_results, name='results'),
 )
+
--- students/admin.py
+++ students/admin.py
@@ -1,26 +1,19 @@
 from django.contrib import admin
 from students.models import Student
+from students.models import CourseApplication
 
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ('full_name', 'email', 'skype')
-	search_fields = ('surname', 'email')
+	search_fields = ['surname', 'email']
+	list_display = ['full_name',  'email', 'skype']
 	list_filter = ['courses']
-	filter_horizontal = ['courses']
-
-	def full_name(self, obj):
-		return "%s %s" % (obj.name, obj.surname)
-
-	fieldsets = (
-		('Personal info', {
-			'fields': ('name', 'surname', 'date_of_birth')
-		}), 
-		('Contact info', {
-			'fields': ('email', 'phone', 'address', 'skype')
-		}),
-		(None, {
-			'fields': ('courses', ),
-		}), 
-	)
+	
+	fieldsets = [
+        ('Personal info',               {'fields': ['name', 'surname', 'date_of_birth']}),
+        ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+		(None, {'fields': ['courses']}),
+    ]
+	filter_horizontal = [ 'courses' ]
 
 admin.site.register(Student, StudentAdmin)
+admin.site.register(CourseApplication)
