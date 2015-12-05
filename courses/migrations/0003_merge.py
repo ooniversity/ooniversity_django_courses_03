65a19cde6513595223ec012d41e3ea07cd65f513
ibeltek <igorbeltek@ukr.net>
2015-12-05 16:10:55 -0500

    7.1_v2

---
+++ courses/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0002_auto_20151128_1910'),
+        ('courses', '0002_auto_20151128_2334'),
+    ]
+
+    operations = [
+    ]
