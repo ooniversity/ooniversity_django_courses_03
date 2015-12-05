a875ef787d269cdacd2872779673018bccf5f5ac
Ilona-Barenshtein <ilonabaren@gmail.com>
2015-12-05 20:28:53 +0200

    Lesson1

--- courses/urls.py
+++ courses/urls.py
@@ -7,6 +7,9 @@ from courses import views
 urlpatterns = patterns('',
     
     url(r'^([0-9]+)/$', views.courses, name='detail'),
-    
-   
+    url(r'^edit/(?P<course_id>\d+)/', views.edit, name="edit"),
+    url(r'^add/$', views.add, name="add"),
+    url(r'^remove/(?P<course_id>\d+)/', views.remove, name="remove"),
+    url(r'^(?P<course_id>[1-9]+)/add_lesson', views.add_lesson, name="add-lesson"),
+
 )
