a3688d1f4e11c42d2260d912476f06efa05ce4c8
OlenaVakulenko <send.olena@gmail.com>
2015-12-05 23:21:58 +0200

    Url name changed from create to add

--- students/urls.py
+++ students/urls.py
@@ -5,7 +5,7 @@ from students.views import list_view, detail, create, edit, remove
 urlpatterns = patterns('',
     url(r'^$', list_view, name='list_view'),
     url(r'^(?P<pk>\d+)/$', detail, name='detail'),
-    url(r'^add/$', create, name='create'),
+    url(r'^add/$', create, name='add'),
     url(r'^edit/(?P<pk>\d+)/$', edit, name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', remove, name='remove'),
     )
