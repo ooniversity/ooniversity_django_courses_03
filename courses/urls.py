301e1f5ff4d6ea7e8814fb7d4c31b53aa8cf667a
Stas mykhailov <s.m.mykhailov@gmail.com>
2015-12-05 03:15:32 +0200

    8-9

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from courses import views
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name = 'detail'),
-    url(r'^add/$', views.add, name='add'),
+    url(r'^add$', views.add, name='add'),
     url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
     url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),
     url(r'^(?P<pk>[0-9]+)/add_lesson$', views.add_lesson, name = 'add-lesson'),
