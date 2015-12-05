d2d44c4fbd40d4907720a6babf4fa9453b6d0b60
Ostrenko <mr.ostrenko@gmail.com>
2015-12-05 21:19:32 +0200

    7_2 Second try

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,13 +1,10 @@
 # -*- coding: UTF-8 -*-
 from django import forms
 
-
 class QuadraticForm(forms.Form):
-
-    a = forms.FloatField(label="коэффициент c")
+    a = forms.FloatField(label="коэффициент a")
     b = forms.FloatField(label="коэффициент b")
     c = forms.FloatField(label="коэффициент c")
-
     def clean_a(self):
         a = self.cleaned_data['a']
         if a == 0:
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,8 +1,5 @@
 #-*-coding: utf-8-*-
-#from django.conf.urls import patterns, include, url
 from django.shortcuts import render
-#from django.http import HttpResponseRedirect, HttpResponse
-#from django.core.urlresolvers import reverse
 from django.views import generic
 from django import forms
 from forms import QuadraticForm
@@ -16,26 +13,20 @@ def quadratic_results(request):
             a = int(form['a'].value())
             b = int(form['b'].value())
             c = int(form['c'].value())
-            discr = b ^ 2 - 4 * a * c
+            discr = b ** 2 - 4 * a * c
             if discr == 0:
                 x1 = - b / (2.0 * a)
                 discr_msg = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {0}".format(x1)
-
             elif discr < 0:
                 discr_msg = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-
             else:
-                x1 = (- b + discr ** (1/2.0)) / (2 * a)
-                x2 = (- b - discr ** (1/2.0)) / (2 * a)
+                x1 = (- b + discr ** (0.5)) / (2 * a)
+                x2 = (- b - discr ** (0.5)) / (2 * a)
                 x1 = round(x1, 1)
                 x2 = round(x2, 1)
-                round(x1, 1)
                 discr_msg = "Квадратное уравнение имеет два действительных корня: x1 = {0}, x2 = {1}".format(x1, x2)
             context['discr_msg'] = discr_msg
             context['discr'] = discr
-
-
         elif form['a'].value() == form['b'].value() == form['c'].value() == None:
             context['form'] = QuadraticForm()
-
     return render(request, 'results.html', context)
