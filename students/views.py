26226711b7aedae526e7b5a1df7f9b7f7682608d
san <san@local>
2015-12-06 01:57:09 +0200

    (8.1) - (---) - Added edit and remove functionality

--- students/views.py
+++ students/views.py
@@ -30,7 +30,7 @@ def create(request):
         if form.is_valid():
             form_content = form.cleaned_data
             form.save()
-            notification = "Student %s %s has been successfully added." % (form_content['name'].encode('utf-8'), form_content['surname'].encode('utf-8'))
+            notification = "Student %s %s has been successfully added." % (form_content['name'], form_content['surname'])
             messages.success(request, notification)
         return redirect('students:list_view')
     else:
