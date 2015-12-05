a91b27c3167c70ed48568db998ea47f598eea344
olegpy <oleg_kriklivets@mail.ru>
2015-12-05 18:37:54 +0200

    edit ex 8

---
+++ courses/migrations/0009_auto_20151205_1637.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0008_auto_20151128_1334'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='short_description',
+            field=models.CharField(max_length=50),
+            preserve_default=True,
+        ),
+    ]
--- students/models.py
+++ students/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     surname = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     skype = models.CharField(max_length=200)
     courses = models.ManyToManyField('courses.Course')
