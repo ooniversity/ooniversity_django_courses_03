b7503b2fae668895cac136bc1c285cf563861fe7
san <san@local>
2015-12-05 18:24:34 +0200

    rewrite quadratic with form (7.2) multiple fixes

--- quadratic/views.py
+++ quadratic/views.py
@@ -9,7 +9,7 @@ def quadratic_results(request):
     quad_form = QuadraticForm(request.GET)
     context = {}
     ##  and any(coef != '' for coef in form)
-    if quad_form.is_valid() and any(coef != '' for coef in quad_form):
+    if quad_form.is_valid():
         a = quad_form.cleaned_data['a']
         b = quad_form.cleaned_data['b']
         c = quad_form.cleaned_data['c']
@@ -28,14 +28,14 @@ def quadratic_results(request):
             context['res'] = u'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
         elif discr == 0:  # a=1&b=6&c=9
             x1 = -b / 2.0 * a
-            context[
-                'res'] = u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f' % x1
+            context['res'] = u'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f' % x1
         else:  # a=-1&b=2&c=35
             x1 = (-b + discr ** (1 / 2.0)) / (2 * a)
             x2 = (-b - discr ** (1 / 2.0)) / (2 * a)
             context['res'] = u'Квадратное уравнение имеет два действительных корня: x1 = %.2f, x2 = %.2f' % (x1, x2)
 
-    else:
+    elif any(coef == '' for coef in quad_form):
+        context['res'] = u'Да вы ебанулись'
         quad_form = QuadraticForm()
 
     context['form'] = quad_form
