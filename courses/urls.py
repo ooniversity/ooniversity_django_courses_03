5f27df3049d52a43558c7f770dcdc7f08a907e0f
bobsley <bobsley345@mail.ru>
2015-12-05 16:19:17 +0200

    H.Task 8 (v1.0)

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from courses.views import detail
+from courses.views import detail, create, edit, remove, add_lesson
 
 urlpatterns = patterns('',
-	url(r'^(?P<course_id>\d)/$', detail, name="detail")
-	
+	#url(r'^$', list_view),
+	url(r'^(?P<course_id>\d+)/$', detail, name="detail"),
+	url(r'^add/$', create, name="add"),	
+	url(r'^edit/(?P<course_id>\d+)/$', edit, name="edit"),
+	url(r'remove/(?P<course_id>\d+)/$', remove, name="remove"),
+	url(r'^(?P<course_id>\d+)/add_lesson$', add_lesson, name='add-lesson'),
 )
+
