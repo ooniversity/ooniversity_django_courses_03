a8cc596fe7328341561ba5dcafe14256ab449ab4
msinenkiy <msinenkiy@gmail.com>
2015-12-05 23:05:00 +0200

    lesson 8.1 done

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,12 @@
-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from courses import views
 
 urlpatterns = patterns('',
-      # url(r'^$', pybursa.views.index, name="index")
-      url(r'^(\d+)/$', views.detail, name = "detail"),
-)
+                       # url(r'^$', pybursa.views.index, name="index")
+                       url(r'^(\d+)/$', views.detail, name="detail"),
+                       url(r'^add/', views.add, name="add"),
+                       url(r'^edit/(?P<course_id>\d+)/$', views.edit, name="edit"),
+                       url(r'^remove/(?P<course_id>\d+)/$', views.remove, name="remove"),
+                       )
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,47 @@
 # -*- coding:UTF-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from courses.models import Course, Lesson
+from courses.forms import CourseModelForm, LessonModelForm
+from django.contrib import messages
 
 
-def detail (request, course_id):
-       details = Course.objects.get(id=course_id)
-       lessons = Lesson.objects.filter(course=course_id)
-       return render (request, 'courses/detail.html',  {'course_detail': details, 'lessons_list': lessons})
+def detail(request, course_id):
+    course = Course.objects.get(id=course_id)
+    lesson = Lesson.objects.filter(course=course_id)
+    return render(request, 'courses/detail.html', {'course_detail': course, 'lessons_list': lesson})
+
+
+def add(request):
+    if request.POST:
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            text = "Course " + form.cleaned_data['name'] + " has been successfully added."
+            messages.success(request, text)
+            return redirect('index')
+    else:
+        form = CourseModelForm()
+    return render(request, 'courses/add.html', {'form': form})
+
+
+def edit(request, course_id):
+    course = Course.objects.get(id=course_id)
+    form = CourseModelForm(instance=course)
+    if request.POST:
+        form = CourseModelForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            text = "The changes have been saved."
+            messages.success(request, text)
+            return redirect('courses:edit', course_id)
+    return render(request, 'courses/edit.html', {'form': form})
+
+
+def remove(request, course_id):
+    course = Course.objects.get(id=course_id)
+    if request.POST:
+        text = "Course " + course.name + " has been deleted."
+        messages.success(request, text)
+        course.delete()
+        return redirect('index')
+    return render(request, 'courses/remove.html', {'course': course})
