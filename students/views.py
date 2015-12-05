bbb97e21e09bfb9755465702a94835246dddf7c2
Dmitry Rusanov <xpertov@gmail.com>
2015-12-05 23:06:42 +0200

    Edit app students

--- students/views.py
+++ students/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from .models import Student
+from students.forms import StudentModelForm
+from django.contrib import messages
+from django.template import RequestContext
+from django.core.context_processors import csrf
 
 
 def list_view(request):
@@ -23,3 +26,46 @@ def detail(request, student_id):
         'student': student,
     }
     return render_to_response('students/detail.html', context)
+
+
+def create(request):
+    context = {}
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            student = form.save()
+            messages.success(request, 'Student %s %s has been successfully added.' % (student.name, student.surname))
+            return redirect('students:list_view')
+    else:
+        form = StudentModelForm()
+    context = {'form': form}
+    context.update(csrf(request))
+    return render_to_response('students/add.html', context, context_instance=RequestContext(request))
+
+
+def edit(request, student_id):
+    student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Information about the student has successfully changed')
+    else:
+        form = StudentModelForm(instance=student)
+    context = {'form': form}
+    context.update(csrf(request))
+    return render_to_response('students/edit.html', context, context_instance=RequestContext(request))
+
+
+def remove(request, student_id):
+    student = Student.objects.get(id=student_id)
+    remove_message = "Warning, the student %s %s will be removed " % (student.name, student.surname)
+    if request.method == 'POST':
+        student.delete()
+        messages.success(request,
+                         "Attention, you're going to remove the student %s %s. Confirm the action " % (
+                         student.name, student.surname))
+        return redirect('students:list_view')
+    context = {'remove_message': remove_message}
+    context.update(csrf(request))
+    return render_to_response('students/remove.html', context, context_instance=RequestContext(request))
