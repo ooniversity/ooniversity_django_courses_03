964212c2201280e8b56715cd944d0ac6a5f35363
SilinMykola <silin.nickolay@gmail.com>
2015-12-06 15:20:21 +0200

    KZ 8 1

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from courses.views import detail
+from courses import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<sid>\d)/$', detail, name="detail")
+    url(r'^(?P<pk>\d)/$', views.detail, name="detail"),
+    url(r'^add/$', views.add, name="add"),
+    url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
+    url(r'^(?P<pk>\d+)/add_lesson$', views.add_lesson, name='add_lesson'),
+
   
 )
