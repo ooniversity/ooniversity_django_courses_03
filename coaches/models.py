589a338a47ddf0357e7b8fceb771bcafdbe2e7fc
san <san@local>
2015-12-06 03:07:18 +0200

    (8.1) - (---) - Final pre-push

--- coaches/models.py
+++ coaches/models.py
@@ -5,22 +5,20 @@ from django.contrib.auth.models import User
 
 
 class Coach(models.Model):
-
     user = models.OneToOneField(User)
 
     date_of_birth = models.DateField()
 
-    gender = models.CharField(max_length = 1,
-        choices=(('M', 'Male'), ('F', 'Female')), default='M')
+    gender = models.CharField(max_length=1,
+                              choices=(('M', 'Male'), ('F', 'Female')), default='M')
 
-    phone = models.CharField(max_length = 24)
+    phone = models.CharField(max_length=24)
 
-    address = models.CharField(max_length = 255)
+    address = models.CharField(max_length=255)
 
-    skype = models.CharField(max_length = 128)
+    skype = models.CharField(max_length=128)
 
     description = models.TextField()
-
 
     def __unicode__(self):
         return self.user.username
