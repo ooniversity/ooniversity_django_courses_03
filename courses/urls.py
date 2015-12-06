cd928141315db4846eb588df73281398a9ea4b54
Oleksandr Ianovskyi <Oleksandr.Ianovskyi@harmonicinc.com>
2015-12-06 15:23:38 +0200

    KZ8_1 is done + changed urls

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,7 @@ from courses import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<course_id>\d+)$', views.detail, name='detail'),
-    url(r'^(?P<course_id>\d+)/add-lesson/$', views.add_lesson, name='add-lesson'),
+    url(r'^(?P<course_id>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
     url(r'^add/$', views.add, name='add'),
     url(r'^edit/(?P<course_id>\d+)/$', views.edit, name='edit'),
     url(r'^remove/(?P<course_id>\d+)/$', views.remove, name='remove'),
