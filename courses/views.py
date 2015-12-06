ae9d544a4c1d1316ee6a8586308980a5c78cb3ef
Ostrenko <mr.ostrenko@gmail.com>
2015-12-06 22:52:32 +0200

    8 Add courses

--- courses/views.py
+++ courses/views.py
@@ -17,7 +17,7 @@ def add(request):
             return redirect('index')
     else:
         form = CourseModelForm()
-    return render(request, 'courses:detail', {'form': form})
+    return render(request, 'courses/add.html', {'form': form})
 
 def edit(request, course_id):
     sd = Course.objects.get(id=course_id)
