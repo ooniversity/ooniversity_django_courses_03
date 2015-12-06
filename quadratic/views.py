8d42acb28323acde4cff957bc97e101160a9b9d1
Olga <Folkacat28@gmil.com>
2015-12-06 21:13:58 +0200

    KZ 8

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django import forms
+
+class QuadraticForm(forms.Form):
+    a = forms.FloatField()
+    b = forms.FloatField()
+    c = forms.FloatField()
 
 def quadratic_results(request):
-    print request.GET
-    a = int(request.GET['a'])
-    b = int(request.GET['b'])
-    c = int(request.GET['c'])
-    print a, b, c
+    form = QuadraticForm()
+    #a = int(request.GET['a'])
+    #b = int(request.GET['b'])
+    #c = int(request.GET['c'])
+    #print a, b, c
     x = x1 = x2 = None
     d = b ** 2 - 4 * a * c 
     if d < 0:
@@ -19,4 +25,4 @@ def quadratic_results(request):
         x1 = (-b + d ** (1/2.0)) / (2*a)
         x2 = (-b - d ** (1/2.0)) / (2*a)
         #result = "Дискриминант: %d Уравнение имеет два действительных корня: x1 = %d, x2 = %d" % (d, x1, x2)
-    return render(request, 'results.html', {"a": a, "b": b, "c": c, "d": d, 'x': x, "x1": x1, "x2": x2})
+    return render(request, 'results.html', {"a": a, "b": b, "c": c, "d": d, 'x': x, "x1": x1, "x2": x2, "form": form})
