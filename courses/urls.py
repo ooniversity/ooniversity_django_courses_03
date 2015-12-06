17f53697f12d0c53faa34de093a20bf952b9d4f8
ShchutskayaElena <ENLevchenko@gmail.com>
2015-12-06 22:50:18 +0200

    8.1

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from courses import views
 
 urlpatterns = patterns('',   
-     url(r'^([0-9]+)/$', views.detail, name='detail'),
+	#url(r'^([0-9]+)/$', views.detail, name='detail'),
+ 	url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
+	
+    url(r'^add/$', views.add, name="add"),
+	url(r'^edit/(?P<course_id>\d+)/', views.edit, name="edit"),
+	
+    url(r'^(?P<course_id>[1-9]+)/add_lesson', views.add_lesson, name="add-lesson"),
+
+    url(r'^remove/(?P<course_id>\d+)/', views.remove, name="remove"),
+
 )
