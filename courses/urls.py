64db01a201dcdca0683e6ad1e9f5235e478068ea
Dmytryi Korbanytskyi <korbanitskiy@gmail.com>
2015-12-06 18:16:41 +0200

    task8_v2

--- courses/urls.py
+++ courses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
                        url(r'^add/$', views.add, name='add'),
                        url(r'^edit/([0-9]+)/$', views.edit, name='edit'),
                        url(r'^remove/([0-9]+)/$', views.remove, name='remove'),
-                       url(r'^([0-9]+)/add_lesson/$', views.add_lesson, name='add_lesson'),
+                       url(r'^([0-9]+)/add_lesson$', views.add_lesson, name='add_lesson'),
                        )
