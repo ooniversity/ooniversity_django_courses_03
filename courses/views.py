a4e8100de7f6c03e729fd54f2ebae712b984a77b
Dmitry Rusanov <xpertov@gmail.com>
2015-12-06 15:28:56 +0200

    modified app course

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.http import Http404
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from coaches.models import Coach
 from .models import Course
 from django.core.exceptions import ObjectDoesNotExist
+from courses.forms import CourseModelForm, LessonModelForm
+from django.contrib import messages
+from django.template.context import RequestContext
+from django.core.context_processors import csrf
 
 
 def detail(request, course_id):
@@ -21,3 +24,56 @@ def detail(request, course_id):
 
     }
     return render_to_response('courses/detail.html', context)
+
+
+def add(request):
+    form = CourseModelForm()
+    if request.method == 'POST':
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            add_course = form.save();
+            messages.success(request, u"Course %s has been successfully added." % add_course.name)
+            return redirect('index')
+    context = {'form': form}
+    context.update(csrf(request))
+    return render_to_response('courses/add.html', context, context_instance=RequestContext(request))
+
+
+def remove(request, course_id):
+    course = Course.objects.get(id=course_id)
+    message = u"%s" % course.name
+    if request.method == 'POST':
+        course.delete()
+        messages.success(request, "Course %s has been deleted." % course.name)
+        return redirect('index')
+    context = {'message': message}
+    context.update(csrf(request))
+    return render_to_response(request, 'courses/remove.html', context, context_instance=RequestContext(request))
+
+
+def edit(request, course_id):
+    course = Course.objects.get(id=course_id)
+    if request.method == "POST":
+        form = CourseModelForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The changes have been saved.')
+            return redirect('courses:edit', course_id)
+    else:
+        form = CourseModelForm(instance=course)
+    context = {'form': form}
+    context.update(csrf(request))
+    return render_to_response('courses/edit.html', context, context_instance=RequestContext(request))
+
+
+def add_lesson(request, course_id):
+    form = LessonModelForm(initial={'course': course_id})
+    if request.method == 'POST':
+        form = LessonModelForm(request.POST)
+        if form.is_valid():
+            add_lesson = form.save()
+            messages.success(request, "Lesson %s has been successfully added." % add_lesson.subject)
+            return redirect('courses:detail', course_id)
+    context = {'form': form}
+    context.update(csrf(request))
+    return render_to_response('courses/add_lesson.html', context, context_instance=RequestContext(request))
