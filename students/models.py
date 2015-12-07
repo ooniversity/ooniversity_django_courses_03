54bd76fc8f2bd60077e8cf7943f48f49849c3ecf
Ostrenko <mr.ostrenko@gmail.com>
2015-12-07 03:00:16 +0200

    7_1

--- students/models.py
+++ students/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     #full_name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     email = models.EmailField()
-    phone = models.CharField(max_length=13)
+    phone = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     skype = models.CharField(max_length=255)
     courses = models.ManyToManyField(Course)
