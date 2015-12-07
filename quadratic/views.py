8ac30a4c97ad55561260de25a62be370cb39615c
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-07 20:34:09 +0200

    Added default form validation (required fields)

--- quadratic/views.py
+++ quadratic/views.py
@@ -4,24 +4,20 @@ from quadratic import forms
 
 
 def quadratic_results(request):
+    d = None
     form = forms.QuadraticForm()
-    #print request.POST
-    if request.GET['a']:
-        var_a = request.GET['a']
-    else:
-        var_a = ''
-    if request.GET['b']:
-        var_b = request.GET['b']
-    else:
-        var_b = ''
-    if request.GET['c']:
-        var_c = request.GET['c']
-    else:
-        var_c = ''
-    if int(var_a) != 0:
-        d = int(var_b) ** 2 - 4 * int(var_a) * int(var_c)
-    else:
-        d = None
+    if request.method == "GET":
+        form = forms.QuadraticForm(request.GET)
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            c = form.cleaned_data['c']
+            # print request.POST
+
+            if int(a) != 0:
+                d = int(b) ** 2 - 4 * int(a) * int(c)
+            else:
+                d = None
 
     var_x = ''
     var_x1 = ''
@@ -30,23 +26,26 @@ def quadratic_results(request):
     if d < 0:
         var_x1 = ''
         var_x2 = ''
-        #print "Roots of the equation doesn't exist"
+        # print "Roots of the equation doesn't exist"
         #exit()
     elif d == 0:
-        var_x = -int(var_b) / 2 * int(var_a)
+        var_x = -int(b) / 2 * int(a)
         var_x = float(var_x)
 
     elif d > 0:
-        var_b = int(var_b)
-        var_a = int(var_a)
-        var_x1 = (-var_b + d ** (1/2.0)) / (2 * var_a)
+        b = int(b)
+        a = int(a)
+        var_x1 = (-b + d ** (1 / 2.0)) / (2 * a)
         var_x1 = round(var_x1, 1)
-        var_x2 = (-var_b - d ** (1/2.0)) / (2 * var_a)
+        var_x2 = (-b - d ** (1 / 2.0)) / (2 * a)
         var_x2 = round(var_x2, 1)
-    #d = var_a + int(var_b) + int(var_c)
-    #eq_ans = 'Result {0}'.format(s)
-    #return HttpResponse(eq_ans)
 
-    sl_full = {'discr': d, 'a': var_a, 'b': var_b, 'c': var_c, 'x': var_x, 'x1': var_x1, 'x2': var_x2,}
+    # d = var_a + int(var_b) + int(var_c)
+
+#eq_ans = 'Result {0}'.format(s)
+#return HttpResponse(eq_ans)
+
+    #sl_full = {'discr': d, 'a': a, 'b': b, 'c': c, 'x': var_x, 'x1': var_x1, 'x2': var_x2, }
+    sl_full = {'discr': d, 'x': var_x, 'x1': var_x1, 'x2': var_x2, }
     sl_full['form'] = form
     return render(request, 'quadratic/results.html', sl_full)
