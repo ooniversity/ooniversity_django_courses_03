8ae091520b8bd7b6d169ee94be645a683da096be
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-07 19:29:47 +0200

    Added a=0 validation with clean_a

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,6 +1,16 @@
+# -*- coding: utf-8 -*-
 from django import forms
 
 class QuadraticForm(forms.Form):
-    a = forms.FloatField()
-    b = forms.FloatField()
-    c = forms.FloatField()
+    a = forms.IntegerField(label="коэффициент a")
+    b = forms.IntegerField(label="коэффициент b")
+    c = forms.IntegerField(label="коэффициент c")
+
+    def clean_a(self):
+        data = self.cleaned_data['a']
+        if a == '0':
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+
+        # Always return the cleaned data, whether you have changed it or
+        # not.
+        return data
