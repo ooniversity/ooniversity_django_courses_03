cc3a6d5a9738aec48ce0890f4845e3a63cc5765d
AnikenRaven <d.petrushenko@hotmail.com>
2015-12-07 09:45:45 +0200

    8 ver.8

--- coaches/models.py
+++ coaches/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Coach(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10, default='M', choices=(('M', 'Male'), ('F', 'Female')))
-    phone = models.CharField(max_length=70)
-    address = models.CharField(max_length=70)
-    skype = models.CharField(max_length=70)
+    gender = models.CharField(max_length=1, default='M', choices=(('M', 'Male'), ('F', 'Female')))
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __unicode__(self):
+        return self.user.username
 
     def name(self):
         return self.user.first_name
@@ -22,5 +26,4 @@ class Coach(models.Model):
     def is_staff(self):
         return self.user.is_staff
 
-    def __unicode__(self):
-        return self.user.username
+
