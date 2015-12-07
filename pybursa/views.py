a7daf5dd36cde0f86393fe973222f154d552c2bc
Dmitry Rusanov <xpertov@gmail.com>
2015-12-07 16:24:33 +0200

    Edit app courses

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from courses.models import Course
 
+
 def index(request):
     list_courses = Course.objects.all().order_by('name')
     context = {
         'list_courses': list_courses,
     }
     return render_to_response('index.html', context)
+
 
 def contact(request):
     return render_to_response('contact.html')
