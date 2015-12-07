0107232b85c6fd7a567d58e773459af008751bdf
CoreMain <d3nshapoval@gmail.com>
2015-12-07 20:56:03 +0200

    кз 7_3
    правки

--- students/urls.py
+++ students/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from students.views import list_view, detail, create, edit, remove
+from students import views
 
 urlpatterns = patterns('',
-	
-	url(r'^(?P<student_id>\d)/$', detail, name="detail"),
-	url(r'^', list_view, name="list_view"),
-	url(r'^add/$', create, name="add"),	
-	url(r'^edit/(?P<student_id>\d+)/$', edit, name="edit"),
-	url(r'remove/(?P<student_id>\d+)/$', remove, name="remove"),
-	
+    url(r'^$', views.list_view, name='list_view'),
+    url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'), 
+    url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.edit, name="edit"),
+    url(r'^remove/(?P<student_id>\d+)/$', views.remove, name="remove"),
 )
--- students/views.py
+++ students/views.py
@@ -25,36 +25,34 @@ def detail(request, student_id):
 	return render(request, 'students/detail.html', {'student':student})
 
 def create(request):
-	if request.method == "POST":
-		form = StudentModelForm(request.POST)
-		if form.is_valid():
-			stud = form.save()
-			mes = u'Студент %s %s успешно добавлен' %(stud.name, stud.surname)
-			messages.success(request, mes)
-			return redirect("students:list_view")
-	else:
-		form = StudentModelForm()
-	return render(request, "students/add.html", {'form':form})
-
+  if request.POST:
+    form = StudentModelForm(request.POST)
+    if form.is_valid():
+      form.save()
+      text = "Student %s %s has been successfully added." %(form.cleaned_data['name'], form.cleaned_data['surname'])
+      messages.success(request, text)
+      return redirect('students:list_view')
+  else:
+    form = StudentModelForm()
+  return render(request, 'students/add.html', {'form': form})
+    
 def edit(request, student_id):
-	student = Student.objects.get(id=student_id)
-	if request.method == "POST":
-		form = StudentModelForm(request.POST, instance=student)
-		if form.is_valid():
-			stud = form.save()
-			mes = u'Данные изменены'
-			messages.success(request, mes)
-	else:
-		form = StudentModelForm(instance=student)
-	return render(request, "students/edit.html", {'form':form})
-	
-def remove(request, student_id):
-	student = Student.objects.get(id=student_id)
-	if request.method == "POST":
-		student.delete()
-		mes = u'Студент %s %s был удалён' %(student.name, student.surname)
-		messages.success(request, mes)
-		return redirect("students:list_view")
+    student = Student.objects.get(id=student_id)
+    form = StudentModelForm(instance=student)
+    if request.POST:
+      form = StudentModelForm(request.POST, instance=student)
+      if form.is_valid():
+        form.save()
+        text = "Info on the student has been sucessfully changed."
+        messages.success(request, text)
+    return render(request, 'students/edit.html', {'form': form})    
 
-	return render(request, "students/remove.html", {'name':student.name, 'surname':student.surname})
-	
+def remove(request, student_id):
+    student = Student.objects.get(id=student_id)
+    form = StudentModelForm(instance=student)
+    if request.POST:
+      text = "Info on %s %s has been sucessfully deleted." %(student.name, student.surname)
+      messages.success(request, text)  
+      student.delete()
+      return redirect('students:list_view')
+    return render(request, 'students/remove.html', {'form': form})
