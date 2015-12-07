8c319726487439979235dabbc318a2523db51d86
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-07 19:06:46 +0200

    Quadratic code re-factoring (separated forms.py and views.py)

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 #import views
-import forms
+import views
 
 urlpatterns = patterns('',
     # ex: /polls/
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # ex: /polls/5/results/
     #url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
 
-    url(r'^results/$', forms.quadratic_results, name='results'),
+    url(r'^results/$', views.quadratic_results, name='results'),
     # ex: /polls/5/vote/
     #url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
