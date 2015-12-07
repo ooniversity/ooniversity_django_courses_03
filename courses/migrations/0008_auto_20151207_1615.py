8005914ffdde187901ce06e2c1e90330fc1bad6d
Andrey Rimarenko <andrey.rimarenko@gmail.com>
2015-12-07 16:18:18 +0200

    KZ 8

--- courses/migrations/0008_auto_20151207_1615.py
+++ courses/migrations/0008_auto_20151207_1615.py
@@ -15,18 +15,15 @@ class Migration(migrations.Migration):
             model_name='course',
             name='name',
             field=models.CharField(max_length=100),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='course',
             name='short_description',
             field=models.CharField(max_length=250),
-            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='lesson',
             name='subject',
             field=models.CharField(max_length=100),
-            preserve_default=True,
         ),
     ]
