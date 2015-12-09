6fa4c4c1e02ceffca87a8f5214dbafe7dc044cd3
pogorelov-sergey <sergey.pogorelov.1989@mail.ru>
2015-12-09 20:34:02 +0200

    modified courses and students

--- pybursa/views.py
+++ pybursa/views.py
@@ -5,14 +5,4 @@ def index(request):
 	courses = Course.objects.all()
 	return render(request, 'index.html', {'courses': courses})
 	
-def contact(request):
-	return render(request, 'contact.html')
-'''	
-def student_list(request):
-	return render(request, 'student_list.html')
-	
-def student_detail(request):
-	return render(request, 'student_detail.html')
-'''	
-
 
--- students/views.py
+++ students/views.py
@@ -1,25 +1,80 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render, redirect
 from students.models import Student
 from courses.models import Course
 from students.forms import StudentModelForm
 from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+class StudentListView(ListView):
+    model = Student
 	
-def list_view(request):
-	r = request.GET
-	if 'course_id' in r:
-		students = Student.objects.filter(courses=r['course_id'])
-	else:
-		students = Student.objects.all()
-	return render(request, 'students/list.html', {'students': students})
+    def get_queryset(self):
+	    r = self.request.GET
+	    if 'course_id' in r:
+		    students = Student.objects.filter(courses=r['course_id'])
+	    else:
+		    students = Student.objects.all()
+	    return students
+	
+	    
+class StudentDetailView(DetailView):
+    model = Student
 
-	
-def detail(request, pk):
-	students = Student.objects.filter(id=pk)
-	return render(request, 'students/detail.html', {'students': students})
 
-	
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentModelForm
+    success_url = reverse_lazy('students:list_view') 
+    
+    def get_context_data(self, **kwargs):
+        context = super(StudentCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Student registration'
+        return context
+    
+    def form_valid(self, form):
+        student = form.save()
+        messages.success(self.request, 'Student %s %s has been successfully added.' % (student.name, student.surname))
+        return super(StudentCreateView, self).form_valid(form)
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentModelForm
+    #success_url = reverse_lazy('students:list_view')
+    
+    def get_success_url(self):
+        return reverse('students:edit', kwargs={'pk': self.object.pk})
+    
+    def get_context_data(self, **kwargs):
+        context = super(StudentUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'Student info update'
+        return context
+        
+    def form_valid(self, form):
+        messages.success(self.request, 'Info on the student has been sucessfully changed.')
+        return super(StudentUpdateView, self).form_valid(form)
+        
+      
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students:list_view')
+    
+    def get_context_data(self, **kwargs):
+        context = super(StudentDeleteView, self).get_context_data(**kwargs)
+        context['title'] = 'Student info suppression'
+        return context
+        
+    def delete(self, request, pk, *args, **kwargs):
+        application = Student.objects.get(id=pk)
+        messages.success(self.request, 'Info on %s %s has been sucessfully deleted.' % (application.name, application.surname))
+        return super(StudentDeleteView, self).delete(request, *args, **kwargs)
+
+
+        
+'''   
 def create(request):
 	if request.method == 'POST':	
 		model_form = StudentModelForm(request.POST)
@@ -54,5 +109,4 @@ def remove(request, pk):
 		messages.success(request, 'Info on %s %s has been sucessfully deleted.' % (application.name, application.surname))
 		return redirect('students:list_view')
 	return render(request, 'students/remove.html', {'application': application})	
-	
-		
+'''	
