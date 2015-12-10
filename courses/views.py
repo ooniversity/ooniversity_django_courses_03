63eb543b5862f24f026169ed362aac21c4bfcb55
Dmitry Rusanov <xpertov@gmail.com>
2015-12-10 17:41:59 +0200

    Edit app courses to cbv

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render, get_object_or_404, redirect
-from coaches.models import Coach
+from django.shortcuts import render, redirect
 from .models import Course
-from django.core.exceptions import ObjectDoesNotExist
-from courses.forms import CourseModelForm, LessonModelForm
+from courses.forms import LessonModelForm
 from django.contrib import messages
 from django.views.generic import DeleteView, CreateView, UpdateView, DetailView
 from django.core.urlresolvers import reverse_lazy
@@ -15,31 +13,15 @@ class CourseDetailView(DetailView):
     template_name = 'courses/detail.html'
 
 
-# def detail(request, course_id):
-#     try:
-#         coach = Coach.objects.get(coach_courses__exact=course_id)
-#         assistant = Coach.objects.get(assistant_courses__exact=course_id)
-#     except ObjectDoesNotExist:
-#         coach = False
-#         assistant = False
-#     course = get_object_or_404(Course, pk=course_id)
-#     context = {
-#         'course': course,
-#         'coach': coach,
-#         'assistant': assistant,
-#     }
-#     return render(request, 'courses/detail.html', context)
-
 class CourseCreateView(CreateView):
     model = Course
     fields = '__all__'
-    success_url = reverse_lazy('index')
     template_name = 'courses/add.html'
     context_object_name = 'form'
 
     def get_context_data(self, **kwargs):
         context = super(CourseCreateView, self).get_context_data(**kwargs)
-        context['form_name'] = 'Add new course'
+        context['form_name'] = 'Course creation'
         context['title'] = 'Course creation'
         context['button_text'] = 'Добавить'
         context['button_class'] = 'waves-effect waves-light btn cyan'
@@ -57,14 +39,14 @@ class CourseUpdateView(UpdateView):
     template_name = 'courses/edit.html'
     context_object_name = 'form'
 
-    def get_success_url(self):
-        course = super(CourseUpdateView, self).get_context_data()
-        success_url = reverse_lazy('courses:edit', kwargs={'course_id': course['form'].id})
-        return success_url
+    # def get_success_url(self):
+    #     course = super(CourseUpdateView, self).get_context_data()
+    #     success_url = reverse_lazy('courses:edit', kwargs={'course_id': course['form'].id})
+    #     return success_url
 
     def get_context_data(self, **kwargs):
         context = super(CourseUpdateView, self).get_context_data(**kwargs)
-        context['form_name'] = 'Edit course'
+        context['form_name'] = 'Course update'
         context['title'] = 'Course update'
         context['button_text'] = 'Редактировать'
         context['button_class'] = 'waves-effect waves-light btn'
@@ -83,7 +65,7 @@ class CourseDeleteView(DeleteView):
 
     def get_context_data(self, **kwargs):
         context = super(CourseDeleteView, self).get_context_data(**kwargs)
-        context['form_name'] = 'Remove course'
+        context['form_name'] = 'Course deletion'
         context['remove_message'] = 'Warning, the course %s will be removed!' % context['course'].name
         context['title'] = 'Course deletion'
         return context
