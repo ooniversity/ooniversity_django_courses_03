57af2a7815f3af4b17082523f1d92e2de3599abc
is <is@itsvit.org>
2015-12-10 13:00:31 +0200

    kz9_2 first try

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from courses import views
+from courses.views import *
+
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
-    url(r'^add/$', views.add, name='add'),
-    url(r'^(?P<pk>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
-    url(r'^edit/(?P<pk>\d+)/$', views.edit, name='edit'),
-    url(r'^remove/(?P<pk>\d+)/$', views.remove, name='remove'),
+    url(r'^(?P<pk>\d+)/$', CourseDetailView.as_view(), name='detail'),
+    url(r'^add/$', CourseCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', CourseUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', CourseDeleteView.as_view(), name='remove'),
+    url(r'^(?P<pk>\d+)/add_lesson$', LessonCreateView.as_view(), name='add-lesson')
 )
