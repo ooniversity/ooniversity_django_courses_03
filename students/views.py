baf05e3c06ec05b7ae3e6bb336d909284d4d69d1
Alexander Babichuk <Oleksandr.Babichuk@gmail.com>
2015-12-10 12:12:04 +0200

    Mockup-50

--- students/views.py
+++ students/views.py
@@ -8,7 +8,6 @@ from students.forms import StudentModelForm
 # Create your views here.
 class StudentListView(ListView):
     model = Student
-    context_object_name = 'students'
 
     def get_queryset(self):
         students = super(StudentListView, self).get_queryset()
@@ -21,7 +20,6 @@ class StudentListView(ListView):
 
 class StudentDetailView(DetailView):
     model = Student
-    context_object_name = 'student'
 
 
 class StudentCreateView(CreateView):
