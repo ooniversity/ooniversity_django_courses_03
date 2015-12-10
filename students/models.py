0eb7f7433e26405963adf0f280faa0acc518bb36
SilinMykola <silin.nickolay@gmail.com>
2015-12-10 23:08:22 +0200

    KZ 9_1 2

--- students/models.py
+++ students/models.py
@@ -2,16 +2,21 @@
 import datetime
 from django.db import models
 from courses.models import Course
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 
 class Student(models.Model):
-  name = models.CharField(max_length = 255)
-  surname = models.CharField(max_length = 255)
-  date_of_birth = models.DateField()
-  email = models.EmailField()
-  phone = models.CharField(max_length = 25, null = True, blank = True)
-  address = models.CharField(max_length = 255, null = True, blank = True)
-  skype = models.CharField(max_length = 255)
-  courses = models.ManyToManyField(Course)
-  def __unicode__(self):
-    return ' '.join([self.name,self.surname])
+    name = models.CharField(max_length = 255)
+    surname = models.CharField(max_length = 255)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length = 25, null = True, blank = True)
+    address = models.CharField(max_length = 255, null = True, blank = True)
+    skype = models.CharField(max_length = 255)
+    courses = models.ManyToManyField(Course)
+  
+    def __unicode__(self):
+        return ' '.join([self.name,self.surname])
+
+    def get_absolute_url(self):
+        return reverse('students:list_view')
+
