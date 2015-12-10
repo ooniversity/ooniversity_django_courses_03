bf78437a3b0fe15a7f4ad135f5e89b2be7dcddb1
Dmitry Rusanov <xpertov@gmail.com>
2015-12-10 17:21:58 +0200

    Edit app courses to cbv

--- courses/urls.py
+++ courses/urls.py
@@ -4,10 +4,10 @@ from courses import views
 
 urlpatterns = patterns('',
 
-        url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
-        url(r'^add/$', views.add, name='add'),
-        url(r'^remove/(?P<course_id>\d+)/$', views.remove, name='remove'),
-        url(r'^edit/(?P<course_id>\d+)/$', views.edit, name='edit'),
+        url(r'^(?P<course_id>\d+)/$', views.CourseDetailView.as_view(pk_url_kwarg='course_id'), name='detail'),
+        url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
+        url(r'^remove/(?P<course_id>\d+)/$', views.CourseDeleteView.as_view(pk_url_kwarg='course_id'), name='remove'),
+        url(r'^edit/(?P<course_id>\d+)/$', views.CourseUpdateView.as_view(pk_url_kwarg='course_id'), name='edit'),
         url(r'^(?P<course_id>\d+)/add_lesson$', views.add_lesson, name='add_lesson'),
 
 )
