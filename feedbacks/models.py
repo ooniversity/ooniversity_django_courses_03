5eb0eec29bb98c0d733b7d903646673d51ed0fea
msinenkiy <msinenkiy@gmail.com>
2015-12-11 21:07:09 +0200

    lesson 10.1 is done

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-import datetime
-from django.utils import timezone
 
 
 class Feedback(models.Model):
     name = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
     message = models.TextField()
-    create_date = models.DateTimeField(default=datetime.datetime.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     from_email = models.EmailField()
 
     def __unicode__(self):
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,28 +1,8 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.views import generic
+from django.core.urlresolvers import reverse_lazy
 from django.contrib import messages
-from feedbacks.forms import FeedbackForm
 from feedbacks.models import Feedback
 from django.views.generic.edit import CreateView
 from django.core.mail import mail_admins
-import datetime
-from django.conf import settings
-
-
-# class FeedbackView(CreateView):
-#     model = Feedback
-#     form_class = FeedbackForm
-#     template_name = "feedback.html"
-#     success_url = reverse_lazy('feedback')
-#
-#     def form_valid(self, form):
-#         data = form.cleaned_data
-#         mail_admins(data['subject'], data['message'], fail_silently=True)
-#         messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!',
-#                          extra_tags='msg')
-#         return super(FeedbackView, self).form_valid(form)
 
 
 class FeedbackView(CreateView):
