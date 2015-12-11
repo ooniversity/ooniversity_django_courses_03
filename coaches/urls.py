d3e5e9d742b47caf4c0ce1f8e63431dc2fcc4004
j_lymar <lulenkula@gmail.com>
2015-12-11 23:26:57 +0200

    kz 9 test

--- coaches/urls.py
+++ coaches/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, url
-
-from coaches import views
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from coaches.views import detail
 
 urlpatterns = patterns('',
-    url(r'(?P<teacher_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<teacher_id>\d)/$', detail, name="detail")
+
 )
--- courses/models.py
+++ courses/models.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from coaches.models import Coach
 
 class Course (models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=600)
     description = models.TextField()
     coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
     assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
+
     def  __unicode__(self):
         return self.name
 
 class Lesson (models.Model):
-    subject = models.CharField(max_length=30)
+    subject = models.CharField(max_length=255)
     description = models.TextField()
     course = models.ForeignKey(Course)
     order = models.PositiveIntegerField()
+
     def  __unicode__(self):
         return self.subject
