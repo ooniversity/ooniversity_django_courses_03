2a464e4ea2dcf4ae5e0d6c249696121d251deddb
olegpy <oleg_kriklivets@mail.ru>
2015-12-11 21:11:30 +0200

    ex 10.1

--- students/views.py
+++ students/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.core.paginator import Paginator
 
 from students.models import Student
 
