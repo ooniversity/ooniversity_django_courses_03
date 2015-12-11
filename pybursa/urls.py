d5f7ecb9d709dcb956cfe6007bcb3dca99af1792
ivnukov <dleshui@gmail.com>
2015-12-12 00:37:22 +0200

    kr 10 first blood

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,19 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pybursa import views
+from pybursa.views import index, contact, student_list, student_detail
+from feedbacks import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^contact/', views.contact, name = 'contact'),
-    url(r'^student_list/', views.student_list, name = 'student_list'),
-    url(r'^student_detail/', views.student_detail, name  = 'student_detail'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index, name='index'),
+    url(r'^contact/', contact, name = 'contact'),
+    url(r'^student_list/', student_list, name = 'student_list'),
+    url(r'^student_detail/', student_detail, name  = 'student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quadratic/', include('quadratic.urls'), name = "quadratic"),
     url(r'^students/', include('students.urls', namespace = "students")),
     url(r'^courses/', include('courses.urls', namespace = "courses")),
-    url(r'^coaches/', include('coaches.urls', namespace = "coaches"))
-
+    url(r'^coaches/', include('coaches.urls', namespace = "coaches")),
+    url(r'^feedback/', views.FeedbackView.as_view(), name = 'feedback'),
 #    
 )
