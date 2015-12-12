dd3df5f78f838f605f0de576842f65303cc3f55a
san <san@local>
2015-12-12 23:10:38 +0200

    (9.2) - (---) - Commented old code

--- courses/views.py
+++ courses/views.py
@@ -71,6 +71,20 @@ class CourseDeleteView(DeleteView):
         messages.success(self.request, message)
         return super(CourseDeleteView, self).delete(request, *args, **kwargs)
 
+
+def add_lesson(request, course_id):
+    if request.method == "post":
+        form = LessonModelForm(request.POST)
+        if form.is_valid():
+            tmp = form.save()
+            subject = tmp.subject
+            messages.success(request, 'Lesson %s has been successfully added.' % subject)
+            return redirect('courses:detail', form.cleaned_data['course'].id)
+    else:
+        form = LessonModelForm(initial={'course': course_id})
+    return render(request, 'courses/add_lesson.html', {'form': form})
+
+'''
 # ---- old
 
 def detail(request, course_id):
@@ -112,16 +126,4 @@ def remove(request, course_id):
         messages.success(request, "Course %s has been deleted." % course.name)
         return redirect('index')
     return render(request, 'courses/remove.html', {'course': course})
-
-
-def add_lesson(request, course_id):
-    if request.method == "post":
-        form = LessonModelForm(request.POST)
-        if form.is_valid():
-            tmp = form.save()
-            subject = tmp.subject
-            messages.success(request, 'Lesson %s has been successfully added.' % subject)
-            return redirect('courses:detail', form.cleaned_data['course'].id)
-    else:
-        form = LessonModelForm(initial={'course': course_id})
-    return render(request, 'courses/add_lesson.html', {'form': form})
+'''
