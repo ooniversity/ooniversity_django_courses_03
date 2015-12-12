6f576ffca090f9e60ccc67b3394aacdd0f35652e
Evgeniy Padafa <padafaevgeniy@gmail.com>
2015-12-12 21:05:43 +0200

    fix

--- pybursa/views.py
+++ pybursa/views.py
@@ -29,12 +29,3 @@ class FeedbackView(CreateView):
     template_name = 'feedback.html'
     success_url = reverse_lazy('feedback')
 
-    def form_valid(self, form):
-        send_mail(form.cleaned_data['subject'], form.cleaned_data['message'], form.cleaned_data['from_email'], (('slash@25.cm'),), fail_silently=False)
-        messages.success(self.request, "Thank you for your feedback! We will keep in touch with you very soon!")
-        return super(FeedbackView, self).form_valid(form)  
-
-    def get_context_data(self, **kwargs):
-        context = super(FeedbackView, self).get_context_data(**kwargs)
-        context['title'] = "Fill in your suggestions"
-        return context
