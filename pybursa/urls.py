b205b7ace1cc47a2189a8e421d52bca51275d796
Andrey Rimarenko <andrey.rimarenko@gmail.com>
2015-12-12 20:52:48 +0200

    KZ 10-1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from pybursa import views
-from django.shortcuts import get_object_or_404, render
+from feedbacks.views import FeedbackView
 
 
 urlpatterns = patterns('',
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     url(r'^student_list/$', views.student_list, name='student_list' ),
     url(r'^student_detail/$', views.student_detail, name='student_detail' ),
     url(r'^coaches/', include('coaches.urls', namespace="coaches")),
+    url(r'^feedback/$', FeedbackView.as_view(), name='feedback'),
 )
