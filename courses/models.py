c90f13885257f6ef1336ffd6a4defb5684b915a3
Eugene <eugene.belokon@gmail.com>
2015-12-12 23:26:29 +0200

    page_9.2

--- courses/models.py
+++ courses/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from coaches.models import Coach
+from django.core.urlresolvers import reverse
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     description = models.TextField()
-    coach = models.ForeignKey(Coach, null=True, blank=True, related_name="coach_courses")
-    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name="assistant_courses")
+    coach = models.ForeignKey(Coach, related_name='coach_courses', blank=True, null = True)
+    assistant = models.ForeignKey(Coach, related_name='assistant_courses', blank=True, null = True)
 
-    def __unicode__(self):              # __unicode__ on Python 2
+    def __unicode__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('index')
 
 class Lesson(models.Model):
     subject = models.CharField(max_length=255)
@@ -18,7 +21,5 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course)
     order = models.PositiveIntegerField()
 
-
-    def __unicode__(self):              # __unicode__ on Python 2
+    def __unicode__(self):
         return self.subject
-
--- pybursa/views.py
+++ pybursa/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-
-from courses.models import Course
-
+from courses.views import Course
 
 def index(request):
     courses = Course.objects.all()
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {'courses': courses})
-    return HttpResponse(template.render(context))
-
+    return render(request, 'index.html', {'courses': courses})
 
 def contact(request):
     return render(request, 'contact.html')
-
-class MixinTitle(object):
-    def get_context_data(self, **kwargs):
-        context = super(MixinTitle, self).get_context_data(**kwargs)
-        context['title'] = self.title
-        return context
