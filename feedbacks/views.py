dcbb8406c02ff2c67e8dfae5ba18afaf31e61f93
Denn <denn.terentyev@gmail.com>
2015-12-12 12:33:49 +0200

    KZ10.1 first try

---
+++ feedbacks/views.py
@@ -0,0 +1,16 @@
+from django.contrib import messages
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.core.mail import mail_admins
+from feedbacks.models import Feedback
+
+class FeedbackView(CreateView):
+    template_name = 'feedbacks/feedback.html'
+    model = Feedback
+    success_url = reverse_lazy('feedback')
+
+    def form_valid(self, form):
+        feedback = form.save()
+        messages.success(self.request, u'Thank you for your feedback! We will keep in touch with you very soon!')
+        mail_admins(feedback.subject, feedback.message)
+        return super(FeedbackView, self).form_valid(form) 
--- pybursa/settings.py
+++ pybursa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'courses',
     'students',
     'coaches',
+    'feedbacks',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -90,3 +91,8 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+ADMINS = (('denn', 'katafalk@list.ru'), ('denn', 'denn.terentyev@gmail.com'))
