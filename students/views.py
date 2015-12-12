2ee321d703e4a969d5eba95787054bda8e7193bf
san <san@local>
2015-12-12 19:37:52 +0200

    (9.1) - (---) - attr fix88

--- students/views.py
+++ students/views.py
@@ -73,7 +73,7 @@ class StudentDeleteView(DeleteView):
 # good solution https://stackoverflow.com/a/25325228
 
     def delete(self, request, *args, **kwargs):
-        message = u"Info on %s %s has been sucessfully deleted." % self.get_object().name  # successfully
+        message = u"Info on %s %s has been sucessfully deleted." % request  # successfully
         messages.success(self.request, message)
         return super(StudentDeleteView, self).delete(request, *args, **kwargs)
 
