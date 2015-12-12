ee442a5911506663392d17fb73e373eec42a038f
j_lymar <lulenkula@gmail.com>
2015-12-12 22:54:46 +0200

    kz 10-1 trial

---
+++ courses/migrations/0004_auto_20151212_2201.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0003_auto_20151129_2135'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='name',
+            field=models.CharField(max_length=255),
+        ),
+        migrations.AlterField(
+            model_name='lesson',
+            name='subject',
+            field=models.CharField(max_length=255),
+        ),
+    ]
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from pybursa.views import index, contact, student_list, student_detail
 from pybursa import views
+from feedbacks import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^courses/', include('courses.urls', namespace="courses")),
     url(r'^students/', include('students.urls', namespace="students")),
     url(r'^coaches/', include('coaches.urls', namespace='coaches')),
+    url(r'^feedback/$', views.FeedbackView.as_view(), name="feedback"),
     # url(r'^index/', index, name='index'),
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
