4e684ee0f3d41cb1cc6465d6f6fb1534ce455566
san <san@local>
2015-12-12 13:00:28 +0200

    (9.1) - (---) - added ContactView (short record)

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from pybursa import views
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^admin/', include(admin.site.urls)),
                        # url(r'contact/', views.contact, name='contact'),
-                       url(r'contact/', views.ContactView.as_view(), name='contact'),
+                       # url(r'contact/', views.ContactView.as_view(), name='contact'),
+                       url(r'contact/', views.ContactView.as_view(template_name='contact.html'), name='contact'),
                        url(r'students/', include('students.urls', namespace="students")),
                        url(r'courses/', include('courses.urls', namespace='courses')),
                        url(r'coaches/', include('coaches.urls', namespace="coaches")),
