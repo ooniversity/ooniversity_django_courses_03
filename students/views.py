89c1652961caa973c00daedbf23677ee6886083b
san <san@local>
2015-12-13 22:09:40 +0200

    (---) - (---) - quadr tmpl

--- students/views.py
+++ students/views.py
@@ -11,16 +11,16 @@ from django.core.paginator import Paginator
 
 class StudentListView(ListView):
     model = Student
-    context_object_name = "students_list"
+    # context_object_name = "students_list"
     paginate_by = 2  # http://stackoverflow.com/a/5910325
 
-    def get_context_data(self, **kwargs):
-        students_list = super(StudentListView, self).get_context_data(**kwargs)
-        students_list['course'] = self.request.GET.get('course_id', None)
-        return students_list
+    # def get_context_data(self, **kwargs):
+    #     students_list = super(StudentListView, self).get_context_data(**kwargs)
+    #     students_list['course'] = self.request.GET.get('course_id', None)
+    #     return students_list
 
     def get_queryset(self):
-        students_list = super(StudentListView, self).get_queryset()
+        # students_list = super(StudentListView, self).get_queryset()
         course_id = self.request.GET.get('course_id', None)
         if course_id:
             students_list = students_list.filter(courses__id=course_id)
