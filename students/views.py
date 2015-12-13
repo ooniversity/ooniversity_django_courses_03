a06a63df3ec87329e158bfd3b0287e9645c75a61
is <is@itsvit.org>
2015-12-13 14:16:49 +0200

    kz10_2

--- students/views.py
+++ students/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, redirect
 from students.models import Student
 from django.contrib import messages
 from students.forms import *
-
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class StudentDetailView(DetailView):
@@ -16,14 +15,15 @@ class StudentDetailView(DetailView):
 
 class StudentListView(ListView):
     model = Student
+    paginate_by = 2
 
     def get_queryset(self):
-        course_id = self.request.GET.get('course_id')
+        data = super(StudentListView, self).get_queryset()
+        course_id = self.request.GET.get('course_id', None)
         if course_id:
-            students = Student.objects.filter(courses__id=course_id)
-        else:
-            students = Student.objects.all()
-        return students
+            data = qs.filter(courses__id=course_id)
+        return data
+
 
 
 class StudentCreateView(CreateView):
