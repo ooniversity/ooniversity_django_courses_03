567dcde2f2228f55bf9ef17a39309ed49a0bb5d8
Ostrenko <mr.ostrenko@gmail.com>
2015-12-13 21:59:14 +0200

    10_2 n1

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -3,6 +3,5 @@ from models import *
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ['from_email', 'create_date']
-    search_fields = ['name']
 
 admin.site.register(Feedback, FeedbackAdmin)
