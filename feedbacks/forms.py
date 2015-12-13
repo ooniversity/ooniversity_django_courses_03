3f248df082ba984985f5f1ebeead92cfe2e872ab
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-13 19:01:42 +0200

    Changed class name from FeedbackModelForm on FeedbackForm

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -3,6 +3,6 @@ from feedbacks.models import Feedback
 
 
 # Create your views here.
-class FeedbackModelForm(forms.ModelForm):
+class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
