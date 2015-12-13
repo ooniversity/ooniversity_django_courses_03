6df1772a4d218270345a6dd13f8349cd084de65b
san <san@local>
2015-12-13 18:06:35 +0200

    (10.1) - (---) - megacom

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -7,7 +7,7 @@ class Feedback(models.Model):
     name = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
     message = models.TextField()
-    email = models.EmailField()
+    from_email = models.EmailField()
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
