9c58337cdb4413daa50deada25f0c15f38175586
Kostya <ermine.kostya@gmail.com>
2015-12-13 17:03:28 +0200

    Kz_9_2(1)

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.views.generic.edit import CreateView
 from feedbacks.forms import FeedbackForm
 from django.core.urlresolvers import reverse_lazy
+from django.core.mail import send_mail, mail_admins
 
 
 class FeedbackView(CreateView):
@@ -12,5 +13,10 @@ class FeedbackView(CreateView):
 
     def form_valid(self, form):
         form.save()
+        data = form.cleaned_data
+        print data
+        # send_mail(data['subject'], data['message'], data['from_email'], ['to@example.com'], fail_silently=False)
+        mail_admins(data['subject'], data['message'], )
+
         messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
         return super(FeedbackView, self).form_valid(form)
