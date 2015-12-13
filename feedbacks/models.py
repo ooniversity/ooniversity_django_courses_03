4863c7752ece2dfbdd938773af3e5dba7f98945e
Eugene <eugene.belokon@gmail.com>
2015-12-13 18:15:21 +0200

    task_10

---
+++ feedbacks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    from_email = models.EmailField()
+    create_date = models.DateTimeField(default=timezone.now)
