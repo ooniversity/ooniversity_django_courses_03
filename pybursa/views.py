051ec54ceab50c7f081c2b3d58d1360cf1f5c86d
Svetlana77 <asm77@ukr.net>
2015-12-13 19:33:51 +0200

    KR10

---
+++ pybursa/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from courses.models import Course
+
+def index(request):
+    gcourses = Course.objects.all()
+    return render(request,'index.html', {'gc': gcourses})
+    
+def contact(request):
+    return render(request, 'contact.html')
+
+def student_list(request):
+    return render(request, 'student_list.html')
+
+def student_detail(request):
+    return render(request, 'student_detail.html')
