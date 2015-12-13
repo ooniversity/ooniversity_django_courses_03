f65bac14d833415ffa7388e1ef197a16eb05e0c0
George <george@g-Aspire-7720.(none)>
2015-12-13 13:48:22 +0200

    fix1

---
+++ feedbacks/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import mail_admins
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
+
+from feedbacks.models import Feedback
+from feedbacks.forms import FeedbackForm
+
+class FeedbackView(CreateView):
+    model = Feedback
+    form_class = FeedbackForm
+    exclude = ['create_date']
+    template_name = 'feedback.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FeedbackView, self).get_context_data(**kwargs)
+        context['title'] = "Leave a feedback"
+        return context
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        mail_admins(data['subject'], data['message'])
+        messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
+        self.success_url = reverse_lazy('feedback')
+        return super(FeedbackView, self).form_valid(form)
