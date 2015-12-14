4893cc76df661e462b7db9dee0dc876752a62870
Dmitry Rusanov <xpertov@gmail.com>
2015-12-14 15:15:55 +0200

    Edit settings

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '=$94go7r314=w%e=m!e--g^xn@&(cvu-0lu&5+lj#q3z34w3gw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
