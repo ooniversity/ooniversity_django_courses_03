e9664e44df47a2fbed52f65e46c0f5b7cacae762
ShchutskayaElena <ENLevchenko@gmail.com>
2015-12-17 23:17:05 +0200

    11.1.4

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -132,7 +132,7 @@ LOGGING = {
 	'formatters': 
 	{
 		'verbose': {
-			'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(message)s'
+			'format': '%(levelname)s %(asctime)s %(module)s %(funcName)d %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
