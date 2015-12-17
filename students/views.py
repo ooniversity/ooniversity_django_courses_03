e2c62313f4bd16242b42e7fd810630b31b84d891
ivnukov <dleshui@gmail.com>
2015-12-17 15:12:54 +0200

    kr 11/1 - loggers created

--- students/views.py
+++ students/views.py
@@ -9,6 +9,9 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from django.core.paginator import Paginator
+import logging
+
+logger  = logging.getLogger(__name__)
 
 class StudentListView(ListView):
 	model = Student
@@ -31,6 +34,13 @@ class StudentListView(ListView):
 
 class StudentDetailView(DetailView):
 	model = Student
+	def get_context_data(self, **kwargs):
+		context = super(StudentDetailView, self).get_context_data(**kwargs)
+		logger.debug("Students detail view has been debugged")
+		logger.info("Logger of students detail view informs you!")
+		logger.warning("Logger of students detail view warns you!")
+		logger.error("Students detail view went wrong!")
+		return context
 
 class StudentCreateView(CreateView):
 	model = Student
