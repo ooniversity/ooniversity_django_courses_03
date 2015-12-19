6709a4f3ff7cedae7ad684240ec593f92d5c5c62
Ostrenko <mr.ostrenko@gmail.com>
2015-12-19 23:49:09 +0200

    11.2

--- students/tests.py
+++ students/tests.py
@@ -10,3 +10,26 @@ class StudentsListTest(TestCase):
             date_of_birth = '2000-01-01')
         response = self.client.get('/students/')
         self.assertEqual(response.status_code, 200)
+
+    def test_appearance(self):
+        student1 = Student.objects.create(
+            name = 'Ivan',
+            surname = 'Ivanov',
+            date_of_birth = '2000-01-01')
+        response = self.client.get('/students/')
+        self.assertContains(response, 'Ivan Ivanov')
+
+    def test_students_count(self):
+        student1 = Student.objects.create(
+            name = 'Ivan',
+            surname = 'Ivanov',
+            date_of_birth = '2000-01-01')
+        self.assertEqual(Student.objects.all().count(), 1)
+
+    def test_students_empty(self):
+        response = self.client.get('/students/')
+        self.assertContains(response, 'List is empty')
+
+    def test_student_add(self):
+        response = self.client.get('/students/')
+        self.assertContains(response, 'Add')
