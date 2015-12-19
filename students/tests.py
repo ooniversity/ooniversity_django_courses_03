724b6db772577ad84ebfe27fa44d0b8770ffe4e5
Ostrenko <mr.ostrenko@gmail.com>
2015-12-19 23:19:23 +0200

    11.2

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from students.models import Student
 
 # Create your tests here.
+class StudentsListTest(TestCase):
+    def test_200_error(self):
+        student1 = Student.objects.create(
+            name = 'Ivan',
+            surname = 'Ivanov',
+            date_of_birth = '2000-01-01')
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
