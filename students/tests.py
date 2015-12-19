d69f713a667a04cdc2685b2a33a49d4c95524dab
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-19 20:19:09 +0200

    Added more students list tests

--- students/tests.py
+++ students/tests.py
@@ -4,6 +4,51 @@ from students.models import Student
 
 # Create your tests here.
 class StudentsListTest(TestCase):
+    def test_page_students_list(self):
+        from django.test import Client
+        client = Client()
+        student1 = Student.objects.create(
+            name='Ksenia_Test',
+            surname='Kolomiets_Test',
+            date_of_birth='1981-01-29')
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ksenia_Test Kolomiets_Test")
+
+    def test_page_students_list_empty(self):
+        from django.test import Client
+        client = Client()
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No students are available.")
+
+    def test_add_new_student_link_presence(self):
+        from django.test import Client
+        client = Client()
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Add a new student")
+
+    def test_students_list_next_page_link_presence(self):
+        from django.test import Client
+        client = Client()
+        student1 = Student.objects.create(
+            name='Ksenia_Test',
+            surname='Kolomiets_Test',
+            date_of_birth='1981-01-29')
+        student2 = Student.objects.create(
+            name='Ksenia_Test2',
+            surname='Kolomiets_Test2',
+            date_of_birth='1981-01-29')
+        student3 = Student.objects.create(
+            name='Ksenia_Test3',
+            surname='Kolomiets_Test3',
+            date_of_birth='1981-01-29')
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "next >>")
+
+
     def test_student_create(self):
         student1 = Student.objects.create(
             name='Ksenia',
