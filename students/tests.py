d9de94a547f52f429ed54501eeb9096ab595999c
Orest Stetsiak <axeopa@gmail.com>
2015-12-19 15:06:36 +0200

    11.2 tests

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from students.models import Student
 
-# Create your tests here.
+class StudentsListTest(TestCase):
+
+    def test_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_students_page(self):
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_student_page_fails(self):
+        response = self.client.get('/students/1/')
+        self.assertEqual(response.status_code, 404)
+
+
