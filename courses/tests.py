b2d6655d77c0e56761ceb50643e275e1e5de8146
SilinMykola <silin.nickolay@gmail.com>
2015-12-19 17:39:54 +0200

    KZ 11_2 1

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from django.test import TestCase, Client
+from models import Course, Lesson
+from courses.views import add_lesson
 
-# Create your tests here.
+def create_courses():
+        some_courses = {}
+        some_courses['course1'] = Course.objects.create(
+                                name ='Pybursa02',
+                                short_description="Some short description",
+                                description = "Some long description")
+        some_courses['course2'] = Course.objects.create(
+                                name ='Pybursa03',
+                                short_description="Some short description",
+                                description = "Some long description")
+        for course in some_courses:
+            lesson = Lesson.objects.create(
+                                    subject = 'Some Web topic',
+                                    description = 'Good description for our subject',
+                                    course = Course.objects.get(id=some_courses[course].id),
+                                    order = 1)
+        return some_courses
+
+class CoursesListTest(TestCase):
+
+    def test_course_create(self):
+        client = Client()
+        course1 = create_courses()
+        self.assertEqual(Course.objects.all().count(), 2)
+
+    def test_course_page_no_courses(self):
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_course_page_have_courses(self):
+        client = Client()
+        course1 = create_courses()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        #self.assertContains(response, "PyBursa02")
+
+    def test_course_page_have_title(self):
+        client = Client()
+        course = create_courses()
+        for i in course:
+            response = client.get('/courses/%d/' % course[i].id)
+            self.assertContains(response, '<title>%s Description</title>' % course[i].name )
+
+    def test_course_page_have_course_description(self):
+        client = Client()
+        course = create_courses()
+        for i in course:
+            response = client.get('/courses/%d/' % course[i].id)
+            self.assertContains(response, '<h2>%s</h2>' % course[i].description )
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from django.test import TestCase, Client
+from models import Student
+from courses.models import Course
+from courses.tests import create_courses
 
-# Create your tests here.
+
+def create_students():
+    items = {}
+    courses = create_courses()
+    for i in courses:
+        for id in range(1,4):
+            items['student_' + i + str(id)] = Student.objects.create(
+            name = 'student%d' % id,
+            surname = '%dstudent' %id,
+            date_of_birth = '2015-01-0%d' % id,
+            email = 'student_%d@email.com' % id,
+            phone = str(id) * 10,
+            address = 'student %d address' % id,
+            skype = 'student.skype %d' % id 
+            )
+            items['student_' + i + str(id)].courses.add(courses[i].id)
+    return items
+
+
+
+class StudentsDetailTest(TestCase):
+
+    def test_student_detail_not_found(self):
+        client = Client()
+        response = client.get('/students/1/')  
+        self.assertEqual(response.status_code, 404)
+    
+    def test_student_detail_normal_response(self):
+        client = Client()
+        items = create_students()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_student_detail_have_title(self):
+        client = Client()
+        items = create_students()
+        for i in items:
+            response = client.get('/students/%d/' % items[i].id)
+            self.assertContains(response, '<title>Pybursa Students %s %s info</title>' % (items[i].name, items[i].surname))
+
+    def test_student_detail_header(self):
+        client = Client()
+        items = create_students()       
+        for i in items:
+            response = client.get('/students/%d/' % items[i].id)  
+            self.assertContains(response, '<h2 class="sub-header">%s %s</h2>' % ( items[i].name, items[i].surname ))
+
+    def test_student_detail_email(self):
+        client = Client()
+        items = create_students()       
+        for i in items:
+            response = client.get('/students/%d/' % items[i].id)  
+            self.assertContains(response, '<td>%s</td>' % items[i].email)
+            
+
+    
+class StudentsListTest(TestCase):
+
+    def test_student_add_button(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, '<a href="/students/add/')
+
+
+
+
+
