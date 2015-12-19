22320dc7efb7e693ee8393b210dee35a600e3a32
san <san@local>
2015-12-19 22:14:29 +0200

    (11.1) - (050) - logging (changed to *_logger.log)

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+# -*- coding:UTF-8 -*-
 
-# Create your tests here.
+import random, string
+from datetime import date
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from students.models import Student
+from courses.models import Course
+from django.core.urlresolvers import reverse
+
+
+class StudentsListTest(TestCase):
+    courses_number = 3
+    students_number = random.randrange(1, 4)
+
+    def students_create(self):
+        for i in xrange(self.courses_number):
+            course = self.course_create()
+            for j in xrange(self.students_number):
+                rnd_c = "".join([random.choice(string.letters) for i in xrange(5)])
+                rnd_n = "".join([random.choice(string.digits) for i in xrange(11)])
+                student = Student.objects.create(name='test_student',
+                                                 surname=rnd_c,
+                                                 date_of_birth=date.today(),
+                                                 email=rnd_c + '@test.st',
+                                                 phone=rnd_n,
+                                                 address='This is the test address for ' + rnd_c,
+                                                 skype=rnd_c + '_skype',
+                                                 )
+                student.courses.add(course)
+
+    def test_students_presence_on_page(self):
+
+        self.students_create()
+
+        client = Client()
+        response = client.get('/students/')
+        import pdb; pdb.set_trace()
+        self.assertEqual(response.status_code, 200)
