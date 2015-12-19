0b2aaa8335b4b6ab2008f2f07d760b0ccd292dd6
“oleg-tim” <book1978@gmail.com>
2015-12-19 23:40:38 +0200

    12_1_3

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from courses.models import Course
 from django.views.generic.list import ListView
 
@@ -25,17 +26,23 @@ def student_detail(request):
 def student_list(request):
     return render(request,'student_list.html')
 
+'''
 def my_error_404(request):
     return render(request, '404.html', {'message': 'Sorry, page is not found'})
 
 def my_error_500(request):
     return render(request, '500.html', {'message': 'Sorry, internal server error occurred'})
-
 '''
+
 def my_error_404(request):
     response = render_to_response('404.html', { 'message' : 'Sorry, page is not found' },
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
-'''
+
+def my_error_500(request):
+    response = render_to_response('500.html', { 'message' : 'Sorry, internal server error occurred' },
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
