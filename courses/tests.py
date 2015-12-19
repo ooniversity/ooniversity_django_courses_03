b8f85e24220da332d757879ffa25b6dfadbd3f15
san <san@local>
2015-12-19 19:00:10 +0200

    (11.1) - (050) - logging

--- courses/tests.py
+++ courses/tests.py
@@ -78,4 +78,4 @@ class CoursesListTest(TestCase):
         course_create(course_name)
         # import pdb; pdb.set_trace()
         request = client.get('/')
-        self.assertContains(request.values('courses'), course_name)
+        self.assertContains(request.courses, course_name)
