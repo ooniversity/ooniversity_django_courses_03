4201e87747809e9628a121b2860589192c8edc89
san <san@local>
2015-12-20 01:04:53 +0200

    (11.1) - (050) - logging (changed to *_logger.log)

--- courses/tests.py
+++ courses/tests.py
@@ -16,69 +16,68 @@ from django.core.urlresolvers import reverse
 #             self.courses_number = 1
 
 
-def user_create(name):
-    """ https://docs.djangoproject.com/en/1.7/ref/contrib/auth/#user """
-    prefix = 'test_user_'
-    username = prefix + name
-    email = prefix + name + "@test.ua"
-    first_name = prefix + "first_name"
-    last_name = prefix + "last_name"
-    password = prefix + "password"
-    user = User.objects.create_user(username=username,
-                                    email=email,
-                                    first_name=first_name,
-                                    last_name=last_name,
-                                    password=password,
-                                    )
-    return user
-
-
-def coach_create(coach):
-    prefix = 'test_coach_'
-    date_of_birth = date.today()
-    gender = random.choice('MF')
-    phone = "".join([random.choice(string.digits) for i in xrange(11)])
-    address = "This is the test address"
-    skype = prefix + "skype"
-    description = "This is the test description"
-    coach = Coach.objects.create(user=user_create(coach),
-                                 date_of_birth=date_of_birth,
-                                 gender=gender,
-                                 phone=phone,
-                                 address=address,
-                                 skype=skype,
-                                 description=description,
-                                 )
-    return coach
-
-
-def course_create(course):
-    name = course
-    short_description = "This is the test short_description"
-    description = "This is the test full description"
-    coach = coach_create("".join([random.choice(string.letters) for i in xrange(5)]))
-    assistant = coach_create("".join([random.choice(string.letters) for i in xrange(5)]))
-
-    course = Course.objects.create(name=name,
-                                   short_description=short_description,
-                                   description=description,
-                                   coach=coach,
-                                   assistant=assistant,
-                                   )
-    return course
-
-
-def courses_generator(number):
-    courses_number = number
-
-    for i in range(courses_number):
-        course_name = ("test_course_" + "".join([random.choice(string.letters) for i in xrange(5)]))
-        course_create(course_name)
-
-
 class CoursesListTest(TestCase):
 
-    courses_generator(3)
+    def user_create(self, name):
+        """ https://docs.djangoproject.com/en/1.7/ref/contrib/auth/#user """
+        prefix = 'test_user_'
+        username = prefix + self.name
+        email = prefix + self.name + "@test.ua"
+        first_name = prefix + "first_name"
+        last_name = prefix + "last_name"
+        password = prefix + "password"
+        user = User.objects.create_user(username=username,
+                                        email=email,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        )
+        return user
+
+    def coach_create(self, name):
+        prefix = 'test_coach_'
+        name = prefix + name
+        date_of_birth = date.today()
+        gender = random.choice('MF')
+        phone = "".join([random.choice(string.digits) for i in xrange(11)])
+        address = "This is the test address"
+        skype = prefix + "skype"
+        description = "This is the test description"
+        coach = Coach.objects.create(user=self.user_create(name),
+                                     date_of_birth=date_of_birth,
+                                     gender=gender,
+                                     phone=phone,
+                                     address=address,
+                                     skype=skype,
+                                     description=description,
+                                     )
+        return coach
+
+    def courses_create(self, courses_number):
+
+        courses_list = []
+
+        short_description = "This is the test short_description"
+        description = "This is the test full description"
+
+        for i in range(courses_number):
+
+            random_name = "".join([random.choice(string.letters) for i in xrange(5)])
+
+            coach = self.coach_create(random_name)
+
+            assistant = self.coach_create(random_name + '_a')
+
+            course = Course.objects.create(name=random_name,
+                                           short_description=short_description,
+                                           description=description,
+                                           coach=coach,
+                                           assistant=assistant,
+                                           )
+
+            courses_list.append(course)
+
+        return courses_list
 
     def test_getting_index_page(self):
         client = Client()
@@ -93,12 +92,12 @@ class CoursesListTest(TestCase):
 
     def test_courses_presence_on_page(self):
 
-        # self.courses_generator(self.courses_number)
+        self.courses_generator(3)
 
         client = Client()
         response = client.get('/')
         context_c = response.context['courses']
-        # import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(context_c), self.courses_number)
 
