459e47414798ca1e4bba68062f26e65b21178c05
Alexander Babichuk <Oleksandr.Babichuk@gmail.com>
2015-12-20 16:19:11 +0200

    Mockup-93

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 # from django.http import HttpResponse, HttpResponseNotFound
 from courses.models import *
 
@@ -22,3 +23,10 @@ def student_detail(request):
 
 def base(request):
     return render(request, 'base.html')
+
+
+def my_error_404(request):
+    response = render_to_response('404.html', { 'message' : 'Sorry, page is not found' },
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
