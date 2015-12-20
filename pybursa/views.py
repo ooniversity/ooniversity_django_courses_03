88b8991a959a9ef4c5cd8bca0e61663e6419f52e
Kostyantin <k_moroz@hotmail.com>
2015-12-20 17:34:53 +0200

    12v6

--- pybursa/views.py
+++ pybursa/views.py
@@ -16,13 +16,13 @@ class IndexView(TemplateView):
         context['courses'] = Course.objects.all()
         return context
 '''
-def server_error(request):
+def handler404(request):
     response = render_to_response('errors/404.html', { 'message' : 'Sorry, page is not found' },
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
-def server_error(request):
+def handler500(request):
     response = render_to_response('errors/500.html', { 'message' : 'Sorry, internal server error occurred' },
                                   context_instance=RequestContext(request))
     response.status_code = 500
