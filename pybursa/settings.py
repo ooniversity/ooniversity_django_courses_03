3767a4f838a68ad332aee27eda22064cd5a36fa3
Alexander Babichuk <Oleksandr.Babichuk@gmail.com>
2015-12-20 16:29:26 +0200

    Mockup-94

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'tlhs(t9_51w_ic9*$#7c98hz9cy*v#minphednqj+1m#g19hev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
