0e62974d181bc11cacfad7a4764d2818475c1429
san <san@local>
2015-12-20 18:50:32 +0200

    (12.0) - (***) - collecting statics

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -102,14 +102,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
                        "django.contrib.staticfiles.finders.AppDirectoriesFinder"
                        )
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_files'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
