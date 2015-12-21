38fa7d49e76e2b3a97ef0c578aee9ee63791db8b
Ksenia Kolomiets <ksenia.kolomiets@gmail.com>
2015-12-21 23:58:50 +0200

    Changed [] on () in STATIC_ROOT

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(os.path.dirname(__file__), "assets")]
+STATIC_ROOT = (os.path.join(os.path.dirname(__file__), "assets"))
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 STATICFILES_DIRS = [os.path.join(os.path.dirname(__file__), "static")]
