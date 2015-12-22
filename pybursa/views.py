8c71db455ee961ba6773e32bce32b8e9e4c4758e
SilinMykola <silin.nickolay@gmail.com>
2015-12-22 15:08:33 +0200

    KZ 12.1 2

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from courses.models import Course
+from django.template import RequestContext
 
 
 def index(request):
