3d46fe9557ea0299cecd8da884e3e54297e4efbb
SilinMykola <silin.nickolay@gmail.com>
2015-12-22 15:47:35 +0200

    KZ 12.1 4

--- pybursa/views.py
+++ pybursa/views.py
@@ -25,6 +25,6 @@ def page_not_found(request):
     return response
 
 def server_error(request):
-    response = render_to_response('errors/500.html', {}, context_instance=RequestContext(request))
+    response = render_to_response('500.html', {}, context_instance=RequestContext(request))
     response.status_code = 500
     return response
