faa875a696aa15e39687530d07ca89bb02b7ea54
CoreMain <d3nshapoval@gmail.com>
2015-12-22 21:33:05 +0200

    кз 12_1
    12_1

--- pybursa/views.py
+++ pybursa/views.py
@@ -17,15 +17,3 @@ def student_list(request):
 def student_detail(request):
 	return render(request, 'student_detail.html')
 
-def not_found(request):
-    response = render_to_response('errors/404.html', { 'message' : 'Sorry, page is not found' },
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def server_error(request):
-    response = render_to_response('errors/500.html', { 'message' : 'Sorry, internal server error occurred' },
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
