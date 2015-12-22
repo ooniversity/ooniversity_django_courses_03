c2569005420fbf5f06c23404f0ec91e4d332b0aa
Dmitry Rusanov <xpertov@gmail.com>
2015-12-22 10:49:00 +0200

    KZ11.2

--- pybursa/views.py
+++ pybursa/views.py
@@ -18,14 +18,14 @@ def contact(request):
 
 
 def page_not_found(request):
-    response = render_to_response('404.html', {'message': 'Sorry, page is not found'},
+    response = render_to_response('404_v2.html', {'message': 'Sorry, page is not found'},
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
 
 def server_error(request):
-    response = render_to_response('500.html', {'message': 'Sorry, internal server error occurred'},
+    response = render_to_response('500_v2.html', {'message': 'Sorry, internal server error occurred'},
                                   context_instance=RequestContext(request))
     response.status_code = 500
     return response
