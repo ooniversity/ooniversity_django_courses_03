b6df6d3e6a6f099a095a437fef7df627ad537289
Ostrenko <mr.ostrenko@gmail.com>
2015-12-24 00:53:57 +0200

    site

--- pybursa/wsgi.py
+++ pybursa/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pybursa.settings")
+print '===== sys.path / PYTHONPATH ====='
+for k in sorted(os.environ.keys()):
+    v = os.environ[k]
+    print ('%-30s %s' % (k,v[:70]))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
